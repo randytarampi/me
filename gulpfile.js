03c230de92baca9ba2b991f0866199ebd8aa75d9
Randy Tarampi <randy.tarampi@gmail.com>
2016-09-04 19:40:26 -0700

    Stricter ESLint configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 let gulp = require("gulp");
 let eslint = require("gulp-eslint");
-let mocha = require('gulp-mocha');
+let mocha = require("gulp-mocha");
 
 gulp.task("eslint", function() {
 	return gulp.src(["**/*.js", "!./node_modules/**/*"])
-		.pipe(eslint())
+		.pipe(eslint({fix: true}))
 		.pipe(eslint.format())
 		.pipe(eslint.failOnError());
 });
