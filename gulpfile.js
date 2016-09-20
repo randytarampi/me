ca5128080dbbf78232b3759a538c795b5e9d405e
Randy Tarampi <randy.tarampi@gmail.com>
2016-09-19 19:58:02 -0700

    Add code coverage checks
    Per https://github.com/randytarampi/pseudoimage.woof/commit/a9c2cf5973470e2597538999a1fc8222c044dc4f & https://github.com/randytarampi/pseudoimage.woof/commit/41eb01079f4a4d58b8b76c6b8f0b963da2124d4c

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,15 @@ const gulp = require("gulp");
 const gulpIf = require("gulp-if");
 const eslint = require("gulp-eslint");
 const mocha = require("gulp-mocha");
+const istanbul = require("gulp-istanbul");
+const coveralls = require("gulp-coveralls");
 
 function isFixed(file) {
 	return file.eslint != null && file.eslint.fixed;
 }
 
-gulp.task("eslint", function() {
-	return gulp.src(["**/*.js", "!./node_modules/**/*"])
+gulp.task("eslint", () => {
+	return gulp.src(["**/*.js", "!./node_modules/**/*", "!./coverage/**/*"])
 		.pipe(eslint({fix: true}))
 		.pipe(eslint.format())
 		.pipe(gulpIf(isFixed, gulp.dest("./")))
@@ -19,9 +21,35 @@ gulp.task("eslint", function() {
 
 gulp.task("lint", ["eslint"]);
 
-gulp.task("test.unit", function () {
+gulp.task("test.unit", () =>  {
 	return gulp.src("test/unit/**/*.js", {read: false})
 		.pipe(mocha());
 });
 
 gulp.task("test", ["test.unit"]);
+
+gulp.task("coverage.setup", coverageSetupTask);
+gulp.task("coverage.setup.ci", ["eslint"], coverageSetupTask);
+function coverageSetupTask() {
+	return gulp.src(["lib/**/*.js"])
+		.pipe(istanbul())
+		.pipe(istanbul.hookRequire());
+}
+
+gulp.task("coverage", ["coverage.setup"], coverageTask);
+gulp.task("coverage.ci", ["eslint", "coverage.setup.ci"], coverageTask);
+function coverageTask() {
+	return gulp.src("test/**/*.js", {read: false})
+		.pipe(mocha())
+		.pipe(istanbul.writeReports())
+		.pipe(istanbul.enforceThresholds({thresholds: {global: 80}}));
+}
+
+gulp.task("coveralls", ["coverage"], coverallsTask);
+gulp.task("coveralls.ci", ["eslint", "coverage.ci"], coverallsTask);
+function coverallsTask() {
+	return gulp.src("coverage/**/lcov.info")
+		.pipe(coveralls());
+}
+
+gulp.task("travis", ["eslint", "coveralls.ci"]);
