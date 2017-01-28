710e52e68a6f94e6e534ae3a71637f8b5a318fc2
Randy Tarampi <rt@randytarampi.ca>
2017-01-28 10:12:58 -0400

    Just rely on the common UI components in randytarampi/me.common.js and randytarampi/me.common.jsx.

--- public/components/posts.js
+++ public/components/posts.js
@@ -1,94 +1,9 @@
-import Post from "../../words/post";
-import PostComponent from "./post";
-import autobind from "react-autobind";
-import Infinite from "react-infinite";
-import Dimensions from "react-dimensions";
-import Spinner from "react-spinkit";
-import React from "react";
-import fetch from "isomorphic-fetch";
+import Posts from "me.common.jsx/lib/posts";
 
-class PostsComponent extends React.Component {
-	constructor(props, context, updater) {
-		super(props, context, updater);
-
-		this.state = {
-			page: 1,
-			isLoading: false,
-			posts: []
-		};
-		this.fetch = null;
-
-		autobind(this);
-	}
-
-	load() {
-		if (this.fetch) {
-			return;
-		}
-
-		this.isLoading = true;
-		this.fetch = fetch(`/words?page=${this.state.page}`)
-			.then((response) => {
-				return response.json();
-			})
-			.then((posts) => {
-				this.setState({
-					posts: this.posts.concat(posts.map(Post.fromJSON)),
-					page: this.state.page + 1,
-					isLoading: false
-				});
-				this.fetch = null;
-			});
-	}
-
-	get isLoading() {
-		return !!this.state.isLoading;
-	}
-
-	set isLoading(isLoading) {
-		this.setState({isLoading: isLoading});
-	}
-
-	get posts() {
-		return this.state.posts;
-	}
-
-	render () {
-		const FullPageSpinner = Dimensions({
-			elementResize: true,
-			containerStyle: {
-				margin: 0,
-				padding: 0,
-				border: 0,
-				height: window.innerHeight
-			}
-		})(Spinner);
-
-		return <Infinite
-			useWindowAsScrollContainer={true}
-			elementHeight={500}
-			infiniteLoadBeginEdgeOffset={Infinite.containerHeightScaleFactor(0.05).amount}
-			onInfiniteLoad={this.load}
-			isInfiniteLoading={this.isLoading}
-			loadingSpinnerDelegate={
-				<FullPageSpinner
-					spinnerName="rotating-plane"
-					noFadeIn
-				/>
-			}
-		>
-			{
-				this.posts.map((post) => {
-					return <PostComponent
-						key={`${post.source}-${post.id}`}
-						post={post}
-					/>;
-				})
-			}
-		</Infinite>;
+class PostsComponent extends Posts {
+	get fetchUrl() {
+		return `/words?page=${this.page}`;
 	}
 }
 
-export default Dimensions({
-	elementResize: true
-})(PostsComponent);
+export default PostsComponent;
