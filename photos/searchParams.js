596022511dcdfd1f75d6bcdc323ea25c8111a49e
Randy Tarampi <rt@randytarampi.ca>
2017-01-28 22:25:59 -0400

    Add some basic tests for `SearchParams`.

--- photos/searchParams.js
+++ photos/searchParams.js
@@ -5,7 +5,7 @@ class SearchParams {
 		Object.assign(this, params);
 	}
 
-	to500px() {
+	get F00px() {
 		return _.extend({
 			rpp: this.perPage,
 			sort: "taken_at",
@@ -13,39 +13,87 @@ class SearchParams {
 		}, this);
 	}
 
-	toFlickr() {
+	/**
+	 * @deprecated
+	 * @returns {*}
+	 */
+	to500px() {
+		return this.F00px;
+	}
+
+	get Flickr() {
 		return _.extend({
 			per_page: this.perPage,
 			extras: "url_o, url_k, url_h, url_c, url_z, url_m, url_n, date_upload, date_taken, owner_name, path_alias, description"
 		}, this);
 	}
 
-	toUnsplash() {
+	/**
+	 * @deprecated
+	 * @returns {*}
+	 */
+	toFlickr() {
+		return this.Flickr;
+	}
+
+	get Unsplash() {
 		return _.extend({
 			perPage: this.perPage
 		}, this);
 	}
 
-	toInstagram() {
+	/**
+	 * @deprecated
+	 * @returns {*}
+	 */
+	toUnsplash() {
+		return this.Unsplash;
+	}
+
+	get Instagram() {
 		return _.extend({
 			count: this.perPage
 		}, this);
 	}
 
-	toTumblr() {
+	/**
+	 * @deprecated
+	 * @returns {*}
+	 */
+	toInstagram() {
+		return this.Instagram;
+	}
+
+	get Tumblr() {
 		return _.extend({
 			type: "photo",
 			limit: this.perPage,
-			offset: this.perPage * (!isNaN(this.page) ? this.page - 1 : 0)
+			offset: this.perPage * (this.page - 1)
 		}, this);
 	}
 
+	/**
+	 * @deprecated
+	 * @returns {*}
+	 */
+	toTumblr() {
+		return this.Tumblr;
+	}
+
 	get perPage() {
-		return this.__perPage || 100;
+		return _.isFinite(this.__perPage) ? this.__perPage : 100;
 	}
 
 	set perPage(rpp) {
 		this.__perPage = rpp;
+	}
+
+	get page() {
+		return _.isFinite(this.__page) ? this.__page : 1;
+	}
+
+	set page(page) {
+		this.__page = page;
 	}
 }
 
