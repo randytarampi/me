f5690fb441a7a397dac36c470c53a4ac56d09de1
Randy Tarampi <randy.tarampi@gmail.com>
2017-02-04 13:24:51 -0800

    Hack around the bad refactoring of UI components by using a single `posts` URL.
    See randytarampi/me.common.jsx#aadb434e00d8e334967c746b35a9a6d119d55524

--- me.words.js
+++ me.words.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "dist")));
  * Set up routes
  */
 app.use("/", require("./app/routes"));
-app.use("/words", require("./words/routes"));
+app.use("/posts", require("./words/routes"));
 
 /**
  * Set up @see error404Handler
