randytarampi/me

commit 67b98228845d34f9de7e3ceae26876d10235f96a

Use common components from randytarampi/me.common.js and randytarampi/me.common.jsx.

photos/flickr/photoSource.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const PhotoSource = require("../photoSource");
-const Photo = require("../photo");
+const Photo = require("me.common.js/lib/photo");
 const Creator = require("../creator");
 const SizedPhoto = require("../sizedPhoto");
 const SearchParams = require("../searchParams");

photos/500px/photoSource.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const PhotoSource = require("../photoSource");
-const Photo = require("../photo");
+const Photo = require("me.common.js/lib/photo");
 const Creator = require("../creator");
 const SizedPhoto = require("../sizedPhoto");
 const SearchParams = require("../searchParams");
@@ -92,7 +92,8 @@ class Five00pxSource extends PhotoSource {
 	}
 
 	get isEnabled() {
-		return !!process.env["F00PX_API_KEY"] &&
+		return !process.env.OFFLINE_ONLY &&
+			!!process.env["F00PX_API_KEY"] &&
 			!!process.env["F00PX_API_SECRET"];
 	}
 }

photos/instagram/photoSource.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const PhotoSource = require("../photoSource");
-const Photo = require("../photo");
+const Photo = require("me.common.js/lib/photo");
 const Creator = require("../creator");
 const SizedPhoto = require("../sizedPhoto");
 const SearchParams = require("../searchParams");
@@ -88,7 +88,7 @@ class InstagramSource extends PhotoSource {
 	}
 
 	get isEnabled() {
-		return !!process.env[`${this.type.toUpperCase()}_ACCESS_TOKEN`];
+		return !process.env.OFFLINE_ONLY && !!process.env[`${this.type.toUpperCase()}_ACCESS_TOKEN`];
 	}
 }
 

photos/photoSource.js
@@ -28,7 +28,8 @@ class PhotoSource {
 	}
 
 	get isEnabled() {
-		return !!process.env[`${this.type.toUpperCase()}_API_KEY`] &&
+		return !process.env.OFFLINE_ONLY &&
+			!!process.env[`${this.type.toUpperCase()}_API_KEY`] &&
 			!!process.env[`${this.type.toUpperCase()}_API_SECRET`];
 	}
 }

photos/tumblr/photoSource.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const PhotoSource = require("../photoSource");
-const Photo = require("../photo");
+const Photo = require("me.common.js/lib/photo");
 const Creator = require("../creator");
 const SizedPhoto = require("../sizedPhoto");
 const SearchParams = require("../searchParams");

photos/unsplash/photoSource.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 require("isomorphic-fetch");
 
 const PhotoSource = require("../photoSource");
-const Photo = require("../photo");
+const Photo = require("me.common.js/lib/photo");
 const Creator = require("../creator");
 const SizedPhoto = require("../sizedPhoto");
 const SearchParams = require("../searchParams");

photos/actions.js
@@ -9,7 +9,8 @@ const PHOTO_SOURCES = [
 	require("./flickr/photoSource"),
 	require("./unsplash/photoSource"),
 	require("./instagram/photoSource"),
-	require("./tumblr/photoSource")
+	require("./tumblr/photoSource"),
+	require("./local/photoSource")
 ];
 const INITIALIZED_PHOTO_SOURCES = Promise.all(
 	_.chain(PHOTO_SOURCES)

photos/photo.js
@@ -1,63 +1,0 @@
-const Creator = require("./creator");
-const SizedPhoto = require("./sizedPhoto");
-const Moment = require("moment");
-const _ = require("lodash");
-
-class Photo {
-	constructor(id, source, dateCreated, datePublished, width, height, sizedPhotos, sourceUrl, title, body, creator) {
-		let that = this;
-
-		this.id = id;
-		this.source = source;
-		this.dateCreated = dateCreated && Moment.utc(dateCreated) || datePublished && Moment.utc(datePublished);
-		this.datePublished = datePublished && Moment.utc(datePublished) || this.dateCreated && this.dateCreated.clone();
-		this.width = width;
-		this.height = height;
-		this.sizedPhotos = [];
-		this.sourceUrl = sourceUrl;
-		this.title = title;
-		this.body = body;
-		this.creator = creator;
-
-		_.each(sizedPhotos, (sizedPhoto) => {
-			that.sizedPhoto = sizedPhoto;
-		});
-	}
-
-	static fromJSON(json) {
-		return new Photo(
-			json.id,
-			json.source,
-			json.dateCreated && Moment.utc(json.dateCreated),
-			json.datePublished && Moment.utc(json.datePublished),
-			json.width,
-			json.height,
-			json.sizedPhotos.map(SizedPhoto.fromJSON),
-			json.sourceUrl,
-			json.title,
-			json.body,
-			json.creator && Creator.fromJSON(json.creator)
-		);
-	}
-
-	getSizedPhoto(width) {
-		const sortedSizedPhotos = _.sortBy(this.sizedPhotos, ["width"]);
-		const widthAppropriatePhotos = _.reject(sortedSizedPhotos, (sizedPhoto) => {
-			return sizedPhoto.width < width;
-		});
-		return _.first(widthAppropriatePhotos) || _.last(sortedSizedPhotos);
-	}
-
-	set sizedPhoto(sizedPhoto) {
-		if (!sizedPhoto.height) {
-			sizedPhoto.height = scaleHeightToWidth(sizedPhoto.width, this.width, this.height);
-		}
-		this.sizedPhotos.push(sizedPhoto);
-	}
-}
-
-module.exports = Photo;
-
-function scaleHeightToWidth(limitedWidth, originalWidth, originalHeight) {
-	return ~~((originalHeight / originalWidth) * limitedWidth);
-}

commit 8fb48820d01259d1524ca9f74a6eef75cc28ec8f

Fix regression introduced in 67b98228845d34f9de7e3ceae26876d10235f96a.

photos/flickr/photoSource.js
@@ -74,7 +74,7 @@ class FlickrSource extends PhotoSource {
 			json.id,
 			null,
 			this.type,
-			json.dateCreated,
+			json.datetaken,
 			Moment(parseInt(json.dateupload, 10) * 1000),
 			json.width_o,
 			json.height_o,