randytarampi/me

commit f579b4d12df31090373c1a74af41c3fea8c185f4

I didn't mean to commit these in 03a045ea8b1228b43f81050044dfe93c6479b192.

photos/instagram/photoSource.js
@@ -88,7 +88,7 @@ class InstagramSource extends PhotoSource {
 	}
 
 	get isEnabled() {
-		return !process.env.OFFLINE_ONLY && !!process.env[`${this.type.toUpperCase()}_ACCESS_TOKEN`];
+		return !process.env[`${this.type.toUpperCase()}_ACCESS_TOKEN`];
 	}
 }
 

photos/500px/photoSource.js
@@ -92,8 +92,7 @@ class Five00pxSource extends PhotoSource {
 	}
 
 	get isEnabled() {
-		return !process.env.OFFLINE_ONLY &&
-			!!process.env["F00PX_API_KEY"] &&
+		return !!process.env["F00PX_API_KEY"] &&
 			!!process.env["F00PX_API_SECRET"];
 	}
 }

photos/photoSource.js
@@ -28,8 +28,7 @@ class PhotoSource {
 	}
 
 	get isEnabled() {
-		return !process.env.OFFLINE_ONLY &&
-			!!process.env[`${this.type.toUpperCase()}_API_KEY`] &&
+		return !!process.env[`${this.type.toUpperCase()}_API_KEY`] &&
 			!!process.env[`${this.type.toUpperCase()}_API_SECRET`];
 	}
 }

commit 8636b0117863d8e31bc38094abab4f126f2ffba4

Fix regression introduced in f579b4d12df31090373c1a74af41c3fea8c185f4.

photos/instagram/photoSource.js
@@ -89,7 +89,7 @@ class InstagramSource extends PhotoSource {
 	}
 
 	get isEnabled() {
-		return !process.env[`${this.type.toUpperCase()}_ACCESS_TOKEN`];
+		return !!process.env[`${this.type.toUpperCase()}_ACCESS_TOKEN`];
 	}
 }
 