61f0d6471cb6183b6690601e4422b9896090e553
Randy Tarampi <rt@randytarampi.ca>
2017-03-18 14:33:22 -0700

    Update dependencies.
    Slightly less blind than last time.

--- gulpfile.js
+++ gulpfile.js
@@ -79,8 +79,6 @@ gulp.task("styles:dev", () => {
 
 gulp.task("vendor", () => {
 	return gulp.src([
-			"node_modules/react/dist/react-with-addons.min.js",
-			"node_modules/react-dom/dist/react-dom.min.js",
 			"node_modules/react-mdl/extra/material.min.js"
 		])
 		.pipe(sourcemaps.init())
@@ -91,8 +89,6 @@ gulp.task("vendor", () => {
 });
 gulp.task("vendor:dev", () => {
 	return gulp.src([
-			"node_modules/react/dist/react-with-addons.js",
-			"node_modules/react-dom/dist/react-dom.js",
 			"node_modules/react-mdl/extra/material.js"
 		])
 		.pipe(sourcemaps.init())
@@ -109,7 +105,6 @@ gulp.task("webpack", function (callback) {
 				"NODE_ENV": JSON.stringify("production")
 			}
 		}),
-		new WebPack.optimize.DedupePlugin(),
 		new WebPack.optimize.UglifyJsPlugin()
 	);
 
@@ -127,7 +122,6 @@ gulp.task("webpack", function (callback) {
 gulp.task("webpack:dev", (callback) => {
 	const webpackConfig = Object.create(require("./webpack.config"));
 	webpackConfig.devtool = "sourcemap";
-	webpackConfig.debug = true;
 
 	WebPack(webpackConfig, function(err, stats) {
 		if (err) {
