9bdcaa2de964d0a33c2b256509d68576633a08d1
Randy Tarampi <rt@randytarampi.ca>
2017-03-19 14:24:13 -0700

    Complete randytarampi/me.photos#18.
    Lol. There we go. So much more realistic now.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ const eslint = require("gulp-eslint");
 const gulp = require("gulp");
 const gutil = require("gulp-util");
 const gulpIf = require("gulp-if");
-const istanbul = require("gulp-istanbul");
 const minifyCss = require("gulp-minify-css");
 const mocha = require("gulp-mocha");
 const sass = require("gulp-sass");
@@ -152,28 +151,7 @@ gulp.task("build:dev", [
 	"webpack:dev"
 ]);
 
-gulp.task("coverage.setup", coverageSetupTask);
-gulp.task("coverage.setup.ci", ["eslint"], coverageSetupTask);
-function coverageSetupTask() {
-	return gulp.src(["**/*.js", "!./node_modules/**/*", "!./public/**/*", "!./test/**/*", "!./bin/**/*", "!./coverage/**/*", "!./migrations/**/*"])
-		.pipe(istanbul())
-		.pipe(istanbul.hookRequire());
-}
-
-gulp.task("coverage", ["coverage.setup"], coverageTask);
-gulp.task("coverage.ci", ["eslint", "coverage.setup.ci"], coverageTask);
-function coverageTask() {
-	return gulp.src("test/**/*.js", {read: false})
-		.pipe(mocha())
-		.pipe(istanbul.writeReports())
-		.pipe(istanbul.enforceThresholds({thresholds: {global: 80}}));
-}
-
-gulp.task("coveralls", ["coverage"], coverallsTask);
-gulp.task("coveralls.ci", ["eslint", "coverage.ci"], coverallsTask);
-function coverallsTask() {
+gulp.task("coveralls", () => {
 	return gulp.src("coverage/**/lcov.info")
 		.pipe(coveralls());
-}
-
-gulp.task("travis", ["eslint", "coveralls.ci"]);
+});
