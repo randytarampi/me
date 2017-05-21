234114edec88e67922e6626f6a22e9a00ffb4058
Randy Tarampi <randy.tarampi@gmail.com>
2017-05-21 13:10:17 -0700

    lwip@0.0.9 -> pajk-lwip@0.2.0

--- public/components/photos.js
+++ public/components/photos.js
@@ -1,9 +1,3 @@
-import Posts from "me.common.jsx/lib/posts";
+import WrappedPostsComponent from "me.common.jsx/lib/posts";
 
-class PhotosComponent extends Posts {
-	get fetchUrl() {
-		return `/photos?page=${this.page}`;
-	}
-}
-
-export default PhotosComponent;
+export default WrappedPostsComponent;
