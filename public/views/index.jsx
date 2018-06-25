1a3dae847f37480d0184b7dd856ecf45e24e8f07
Randy Tarampi <rt@randytarampi.ca>
2018-06-25 11:15:45 -0700

    Reduxify.
    Per randytarampi/me.photos#20.

--- public/views/index.jsx
+++ public/views/index.jsx
@@ -1,7 +1,13 @@
-import {Posts} from "me.common.jsx";
+import createBrowserHistory from "history/createBrowserHistory";
+import {configureStore, reducers, ReduxRoot} from "me.common.jsx";
 import React from "react";
 import {render} from "react-dom";
+import routes from "../routes";
+
+const history = createBrowserHistory();
+const store = configureStore(undefined, history, reducers);
 
 render(
-	<Posts/>, document.querySelector(".content")
+	<ReduxRoot store={store} history={history} routes={routes}/>,
+	document.querySelector(".content")
 );
