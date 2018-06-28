d01d3752c1181743ff407343a8828fec9766990c
Randy Tarampi <rt@randytarampi.ca>
2018-06-28 12:32:54 -0700

    Setup Instagram auth via an AWS Lambda instead of an EC2 instance.
    Per randytarampi/me.photos#21.

--- serverless/handler/index.js
+++ serverless/handler/index.js
@@ -1,1 +1,3 @@
 export {default as getPhotos} from "./getPhotos";
+export {default as instagramAuthRedirect} from "./instagramAuthRedirect";
+export {default as instagramAuthReturn} from "./instagramAuthReturn";
