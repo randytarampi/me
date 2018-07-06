eabac4c6dfdb8026b8bb0f3a76918967dbb48fe2
Randy Tarampi <rt@randytarampi.ca>
2018-07-05 20:14:25 -0700

    Don't load serverless secrets if we don't have any.

--- serverless/handler/getPosts.js
+++ serverless/handler/getPosts.js
@@ -1,13 +1,9 @@
 import searchPosts from "../../blog/searchPosts";
-import loadServerlessSecrets from "../util/loadServerlessSecrets";
 import responseBuilder from "../util/responseBuilder";
 import returnErrorResponse from "../util/returnErrorResponse";
 
 export default (event, context, callback) => {
-    loadServerlessSecrets()
-        .then(() => {
-            return searchPosts(event.queryStringParameters)
-                .then(sortedPosts => callback(null, responseBuilder(sortedPosts)));
-        })
+    searchPosts(event.queryStringParameters)
+        .then(sortedPosts => callback(null, responseBuilder(sortedPosts)))
         .catch(returnErrorResponse(callback));
 };
