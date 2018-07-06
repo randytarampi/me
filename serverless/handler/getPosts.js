5719dc0d4dc3b6539dafd8631a65f703618b60a5
Randy Tarampi <rt@randytarampi.ca>
2018-07-06 15:51:19 -0700

    Remove lambdas superseded by randytarampi/me.posts.

--- serverless/handler/getPosts.js
+++
@@ -1,9 +0,0 @@
-import searchPosts from "../../blog/searchPosts";
-import responseBuilder from "../util/responseBuilder";
-import returnErrorResponse from "../util/returnErrorResponse";
-
-export default (event, context, callback) => {
-    searchPosts(event.queryStringParameters)
-        .then(sortedPosts => callback(null, responseBuilder(sortedPosts)))
-        .catch(returnErrorResponse(callback));
-};
--- serverless/util/returnErrorResponse.js
+++
@@ -1,8 +0,0 @@
-import responseBuilder from "./responseBuilder";
-
-export default callback => error => {
-    console.error(error); // eslint-disable-line no-console
-    callback(error, responseBuilder({
-        error: "An unexpected error occurred"
-    }, 500));
-};
