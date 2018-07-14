b8396c43aa2178d6de740a7cb2b56b49fe724d78
Randy Tarampi <rt@randytarampi.ca>
2018-07-14 12:57:28 -0700

    Actually have a loading spinner.

--- lib/components/loadingSpinner.jsx
+++ lib/components/loadingSpinner.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
-const LoadingSpinner = () => <div className="infinite-list-item">
-	Loading...
+const LoadingSpinner = () => <div className="loading-spinner">
+	<div className="preloader-wrapper big active">
+		<div className="spinner-layer spinner-blue">
+			<div className="circle-clipper left">
+				<div className="circle"/>
+			</div>
+			<div className="gap-patch">
+				<div className="circle"/>
+			</div>
+			<div className="circle-clipper right">
+				<div className="circle"/>
+			</div>
+		</div>
+
+		<div className="spinner-layer spinner-red">
+			<div className="circle-clipper left">
+				<div className="circle"/>
+			</div>
+			<div className="gap-patch">
+				<div className="circle"/>
+			</div>
+			<div className="circle-clipper right">
+				<div className="circle"/>
+			</div>
+		</div>
+
+		<div className="spinner-layer spinner-yellow">
+			<div className="circle-clipper left">
+				<div className="circle"/>
+			</div>
+			<div className="gap-patch">
+				<div className="circle"/>
+			</div>
+			<div className="circle-clipper right">
+				<div className="circle"/>
+			</div>
+		</div>
+
+		<div className="spinner-layer spinner-green">
+			<div className="circle-clipper left">
+				<div className="circle"/>
+			</div>
+			<div className="gap-patch">
+				<div className="circle"/>
+			</div>
+			<div className="circle-clipper right">
+				<div className="circle"/>
+			</div>
+		</div>
+	</div>
 </div>;
 
 export default LoadingSpinner;
