5751cda871193b5966d820b21f0b6ae0797dbca7
Randy Tarampi <rt@randytarampi.ca>
2018-07-18 11:57:44 -0700

    The me.* monorepo pushes to manyrepos.
    For now just to take advantage of the existing CI/CD infrastructure.

---
+++ bin/split.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PACKAGE=$1
+GITHUB_USER=${GITHUB_USER:=randytarampi}
+GITHUB_PACKAGE_REPO=me.$PACKAGE;
+
+cd ..;
+git subtree split --prefix=packages/$PACKAGE -b $PACKAGE;
+git checkout $PACKAGE;
+git remote add $PACKAGE https://${GH_TOKEN}@github.com/$GITHUB_USER/$GITHUB_PACKAGE_REPO.git
+git status;
+git push --set-upstream $PACKAGE $PACKAGE:master;
+
+git fetch --force --all && git reset --hard origin/master && git checkout master;
