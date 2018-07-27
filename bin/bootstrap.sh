964fecdca2cfd1d67f4cc57ef8b9707029fe256d
Randy Tarampi <rt@randytarampi.ca>
2018-07-27 11:36:12 -0700

    fix(bootstrap): Link `resume` at the top level in order to fix `npm run build`.
    Otherwise `resume-cli` can't find the theme and you're SOL.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -8,8 +8,12 @@ cd $REPO_ROOT;
 npx lerna bootstrap --hoist --no-ci;
 npm rebuild lwip;
 
+# NOTE-RT: Use the same `lwip` for `posts` and `pseudoimage`
 ln -snf $REPO_ROOT/node_modules/lwip/ $REPO_ROOT/packages/posts/node_modules/lwip;
 ln -snf $REPO_ROOT/node_modules/lwip/ $REPO_ROOT/packages/pseudoimage/node_modules/lwip;
+
+# NOTE-RT: Need to symlink `jsonresume-theme-randytarampi` at the top level `node_modules` directory so `resume-cli` can find it
+ln -snf $REPO_ROOT/packages/resume $REPO_ROOT/node_modules/jsonresume-theme-randytarampi
 
 # FIXME-RT: `serverless` expects us to have a full `node_modules` tree living below us here. Can't tell if I want to hack around things by symlinking to the top level modules folder, so I'll just play it safe and install again.
 cd $REPO_ROOT/packages/posts;
