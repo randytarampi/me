13436ad5dafeeb21bb9d712e870ad9c4ca377fa5
Randy Tarampi <rt@randytarampi.ca>
2018-07-27 17:03:46 -0700

    chore(travis): `travis.sh` -> `split-all.sh`.
    So I can run it locally, which is where I really only want to be running this.

---
+++ bin/split-all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+MONOREPO_ROOT=${TRAVIS_BUILD_DIR:=`pwd`/..}
+
+find $MONOREPO_ROOT/packages -maxdepth 1 -mindepth 1 -type d -execdir $MONOREPO_ROOT/bin/split.sh {} \;
--- bin/travis.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find $TRAVIS_BUILD_DIR/packages -maxdepth 1 -mindepth 1 -type d -execdir $TRAVIS_BUILD_DIR/bin/split.sh {} \;
