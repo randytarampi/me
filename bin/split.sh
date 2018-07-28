6d49d1c69353ee82f5a4262bc4566e2655ba87d6
Randy Tarampi <rt@randytarampi.ca>
2018-07-27 17:01:08 -0700

    chore(split): Fold `force-split.sh` into `split.sh`.

--- bin/split.sh
+++ bin/split.sh
@@ -21,8 +21,6 @@ git checkout $MONOREPO_BASE_BRANCH;
 git branch -D $PACKAGE;
 git remote remove $PACKAGE;
 git remote add $PACKAGE $GITHUB_PACKAGE_REPO_URL
-git pull -s subtree $PACKAGE master
-#git subtree pull --squash --message "Update $PACKAGE from $GITHUB_PACKAGE_REPO" --prefix=packages/$PACKAGE $GITHUB_PACKAGE_REPO_URL master;
 git subtree split --prefix=packages/$PACKAGE -b $PACKAGE;
 git checkout $PACKAGE;
 git status;
