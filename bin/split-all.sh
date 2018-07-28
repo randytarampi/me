a904edd364625b3debb3a193a5fa882d64975753
Randy Tarampi <rt@randytarampi.ca>
2018-07-27 22:14:39 -0700

    fix(split): Define `MONOREPO_ROOT` correctly.

--- bin/split-all.sh
+++ bin/split-all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-MONOREPO_ROOT=${TRAVIS_BUILD_DIR:=`pwd`/..}
+MONOREPO_ROOT=${TRAVIS_BUILD_DIR:=`pwd`}
 
 find $MONOREPO_ROOT/packages -maxdepth 1 -mindepth 1 -type d -execdir $MONOREPO_ROOT/bin/split.sh {} \;
