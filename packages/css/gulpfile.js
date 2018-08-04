randytarampi/me

commit e812d8a914397280b80406e365f274b8297173cd

fix(www): Hack around the fontawesome font copying issue.
There's some weirdness with how symlinks are being resolved on the Travis boxes. Since my cookies are done and I've got better things to do tonight, just copy fonts from the top level modules directory. Just punt the investigation to #21 and call it a day assuming this works.

From fea25c83e60511e39bbd4db5a0a0ff164f196dee, etc.

packages/css/gulpfile.js
@@ -22,11 +22,15 @@ gulp.task("clean", (callback) => {
 gulp.task("copy", () => {
     return gulp
         .src([
+            "../../node_modules/materialize-css/dist/fonts/roboto/**",
+            "../../node_modules/@fortawesome/fontawesome-free/sprites/**",
+            "../../node_modules/@fortawesome/fontawesome-free/svgs/**",
+            "../../node_modules/@fortawesome/fontawesome-free/webfonts/**",
             "node_modules/materialize-css/dist/fonts/roboto/**",
             "node_modules/@fortawesome/fontawesome-free/sprites/**",
             "node_modules/@fortawesome/fontawesome-free/svgs/**",
             "node_modules/@fortawesome/fontawesome-free/webfonts/**"
-        ], {resolveSymlinks: true, debug: true})
+        ])
         .pipe(gulp.dest("./dist"));
 });
 

packages/resume/gulpTasks.js
@@ -15,11 +15,15 @@ gulp.task("copy", () => {
     return gulp
         .src([
             "public/assets/**",
+            "../../node_modules/materialize-css/dist/fonts/roboto/**",
+            "../../node_modules/@fortawesome/fontawesome-free/sprites/**",
+            "../../node_modules/@fortawesome/fontawesome-free/svgs/**",
+            "../../node_modules/@fortawesome/fontawesome-free/webfonts/**",
             "node_modules/@randy.tarampi/css/node_modules/materialize-css/dist/fonts/roboto/**",
             "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/sprites/**",
             "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/svgs/**",
             "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/webfonts/**"
-        ], {resolveSymlinks: true, debug: true})
+        ])
         .pipe(gulp.dest("./dist"));
 });
 

packages/www/gulpfile.js
@@ -14,11 +14,15 @@ gulp.task("copy", () => {
     return gulp
         .src([
             "public/assets/**",
+            "../../node_modules/materialize-css/dist/fonts/roboto/**",
+            "../../node_modules/@fortawesome/fontawesome-free/sprites/**",
+            "../../node_modules/@fortawesome/fontawesome-free/svgs/**",
+            "../../node_modules/@fortawesome/fontawesome-free/webfonts/**",
             "node_modules/@randy.tarampi/css/node_modules/materialize-css/dist/fonts/roboto/**",
             "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/sprites/**",
             "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/svgs/**",
             "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/webfonts/**"
-        ], {resolveSymlinks: true, debug: true})
+        ])
         .pipe(gulp.dest("./dist"));
 });
 

commit 1c38756378ad7aa628d89daca7626e6b8548e89e

fix(travis): Fix broken deploy caused by e812d8a914397280b80406e365f274b8297173cd.
The box seemed to explode trying to commit that many files. Technically we only need the web fonts, so just pack those up.

packages/css/gulpfile.js
@@ -23,12 +23,8 @@ gulp.task("copy", () => {
     return gulp
         .src([
             "../../node_modules/materialize-css/dist/fonts/roboto/**",
-            "../../node_modules/@fortawesome/fontawesome-free/sprites/**",
-            "../../node_modules/@fortawesome/fontawesome-free/svgs/**",
             "../../node_modules/@fortawesome/fontawesome-free/webfonts/**",
             "node_modules/materialize-css/dist/fonts/roboto/**",
-            "node_modules/@fortawesome/fontawesome-free/sprites/**",
-            "node_modules/@fortawesome/fontawesome-free/svgs/**",
             "node_modules/@fortawesome/fontawesome-free/webfonts/**"
         ])
         .pipe(gulp.dest("./dist"));

packages/resume/gulpTasks.js
@@ -16,12 +16,8 @@ gulp.task("copy", () => {
         .src([
             "public/assets/**",
             "../../node_modules/materialize-css/dist/fonts/roboto/**",
-            "../../node_modules/@fortawesome/fontawesome-free/sprites/**",
-            "../../node_modules/@fortawesome/fontawesome-free/svgs/**",
             "../../node_modules/@fortawesome/fontawesome-free/webfonts/**",
             "node_modules/@randy.tarampi/css/node_modules/materialize-css/dist/fonts/roboto/**",
-            "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/sprites/**",
-            "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/svgs/**",
             "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/webfonts/**"
         ])
         .pipe(gulp.dest("./dist"));

packages/www/gulpfile.js
@@ -15,12 +15,8 @@ gulp.task("copy", () => {
         .src([
             "public/assets/**",
             "../../node_modules/materialize-css/dist/fonts/roboto/**",
-            "../../node_modules/@fortawesome/fontawesome-free/sprites/**",
-            "../../node_modules/@fortawesome/fontawesome-free/svgs/**",
             "../../node_modules/@fortawesome/fontawesome-free/webfonts/**",
             "node_modules/@randy.tarampi/css/node_modules/materialize-css/dist/fonts/roboto/**",
-            "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/sprites/**",
-            "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/svgs/**",
             "node_modules/@randy.tarampi/css/node_modules/@fortawesome/fontawesome-free/webfonts/**"
         ])
         .pipe(gulp.dest("./dist"));