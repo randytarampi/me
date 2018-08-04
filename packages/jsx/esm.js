ebf16e63c5251ff706230a697d4345e22796fd61
Randy Tarampi <rt@randytarampi.ca>
2018-08-03 17:04:49 -0700

    test(jsx): Export a bunch more things to make the coverage calculation more accurate.
    Setup for #12 and also see if `test` bumps the build version. Don't assume it will but you never know until you try or read the docs.

--- packages/jsx/esm.js
+++ packages/jsx/esm.js
@@ -1,17 +1,37 @@
-import ClientReduxRoot from "./lib/clientReduxRoot";
+import Bear from "./lib/components/bear";
+import Emoji from "./lib/components/emoji";
+import Link from "./lib/components/link";
+import LoadingSpinner from "./lib/components/loadingSpinner";
 import RowBlock from "./lib/components/rowBlock";
+
 import Posts from "./lib/containers/posts";
+
 import reducers from "./lib/data/reducers";
 import selectors from "./lib/data/selectors";
-import ServerReduxRoot from "./lib/serverReduxRoot";
+
 import configureStore from "./lib/store/configureStore";
 
+import logger from "./lib/logger";
+
+import ClientReduxRoot from "./lib/clientReduxRoot";
+import ServerReduxRoot from "./lib/serverReduxRoot";
+
 export {
-    configureStore,
-    Posts,
-    reducers,
-    ClientReduxRoot,
-    ServerReduxRoot,
+    Bear,
+    Emoji,
+    Link,
+    LoadingSpinner,
     RowBlock,
+
+    Posts,
+
+    reducers,
     selectors,
+
+    configureStore,
+
+    logger,
+
+    ClientReduxRoot,
+    ServerReduxRoot
 };
