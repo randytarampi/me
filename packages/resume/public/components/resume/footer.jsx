0626d60b38ce90015d2f96c23178beefdfd67e34
Randy Tarampi <rt@randytarampi.ca>
2018-08-08 00:18:16 -0700

    fix(resume): Use `é`s consistently.
    Signed-off-by: Randy Tarampi <rt@randytarampi.ca>

--- packages/resume/public/components/resume/footer.jsx
+++ packages/resume/public/components/resume/footer.jsx
@@ -8,7 +8,8 @@ export const ResumeFooter = ({resume}) => {
         <div className="container">
             <Row className="row valign-wrapper center-align resume-footer hide-on-screen">
                 <Col s={12}>
-                    <em>Print styles are hard to write, and one page resumés are harder – check out the full copy at <Link
+                    <em>Print styles are hard to write, and one page résumés are harder – check out the full copy
+                        at <Link
                         href={resume.publish_url}>{resume.publish_url}</Link></em>
                 </Col>
             </Row>
