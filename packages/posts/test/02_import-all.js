1abfe103fc3bd17dd87799f2eef92ad56259a598
Randy Tarampi <rt@randytarampi.ca>
2018-08-08 15:14:37 -0700

    ci(posts): `import` each serverless handler instead of just the `handlers/index`.
    The `require`s are deferred which means they don't actually run and require all the code I want for the coverage calculations.

--- packages/posts/test/02_import-all.js
+++ packages/posts/test/02_import-all.js
@@ -1,1 +1,5 @@
-import "../serverless/handler";
+import "../serverless/handler/getPhotos";
+import "../serverless/handler/getPosts";
+import "../serverless/handler/getWords";
+import "../serverless/handler/instagramAuthRedirect";
+import "../serverless/handler/instagramAuthReturn";
