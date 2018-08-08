ece11fd0d89f48557c65012307d352c81a41192e
Randy Tarampi <rt@randytarampi.ca>
2018-08-07 23:46:25 -0700

    feat(www): Do the whole SEO thing because a random CV writer told me to.
    Dude, it's all about those rich text cards, but whatever, I guess you were free so I might as well.
    
    Signed-off-by: Randy Tarampi <rt@randytarampi.ca>

--- packages/resume/public/components/resume/index.jsx
+++ packages/resume/public/components/resume/index.jsx
@@ -19,9 +19,27 @@ import ResumeHeader from "./header";
 
 export const Resume = props => <div className="resume">
     <Helmet>
-        <title>
-            {props.resume.basics.name} &mdash; {props.resume.basics.label}
-        </title>
+        <title>{props.resume.basics.name} &mdash; {props.resume.basics.label}</title>
+        <meta itemProp="name" content={props.resume.basics.name}/>
+        <meta name="twitter:title" content={props.resume.basics.name}/>
+        <meta name="og:title" content={props.resume.basics.name}/>
+        <meta name="og:site_name" content={props.resume.basics.name}/>
+        <meta name="description" content={props.resume.basics.label}/>
+        <meta itemProp="description" content={props.resume.basics.label}/>
+        <meta name="twitter:description" content={props.resume.basics.label}/>
+        <meta name="og:description" content={props.resume.basics.label}/>
+        <meta name="image" content="/ʕつ•ᴥ•ʔつ.svg"/>
+        <meta itemProp="image" content="/ʕつ•ᴥ•ʔつ.svg"/>
+        <meta name="twitter:image:src" content="/ʕつ•ᴥ•ʔつ.svg"/>
+        <meta name="og:image" content="/ʕつ•ᴥ•ʔつ.svg"/>
+        <meta name="twitter:site" content="@randytarampi"/>
+        <meta name="twitter:creator" content="@randytarampi"/>
+        <meta name="og:locale" content="en_CA"/>
+        <meta name="fb:admins" content="831915416"/>
+        <meta name="fb:app_id" content="1705404522846104"/>
+        <meta name="og:type" content="website"/>
+        <link rel="canonical" href={props.resume.publish_url}/>
+        <meta name="og:url" content={props.resume.publish_url}/>
     </Helmet>
     <ResumeHeader {...props} />
     <div className="resume-content">
--- packages/www/public/views/main.jsx
+++ packages/www/public/views/main.jsx
@@ -1,12 +1,8 @@
 import React, {Fragment} from "react";
-import {Helmet} from "react-helmet";
 import Content from "./content";
 
 const Main = () =>
     <Fragment>
-        <Helmet>
-            <title>ʕ•ᴥ•ʔﾉ゛</title>
-        </Helmet>
         <Content/>
     </Fragment>;
 
