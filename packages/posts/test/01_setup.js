a48f3ef7608849d1aa2f8ed82daf7aafddce6d24
Randy Tarampi <rt@randytarampi.ca>
2018-08-07 21:40:38 -0700

    ci(posts): `import` all lambdas for more accurate code coverage numbers.
    Making progress towards #12.
    
    Signed-off-by: Randy Tarampi <rt@randytarampi.ca>

--- packages/posts/test/01_setup.js
+++ packages/posts/test/01_setup.js
@@ -1,5 +1,7 @@
 import dynamoose from "dynamoose";
 
+process.env.POSTS_DYNAMODB_TABLE = process.env.POSTS_DYNAMODB_TABLE || `${process.env.NODE_ENV}-posts`;
+
 if (process.env.IS_OFFLINE || process.env.NODE_ENV === "test") {
     dynamoose.AWS.config.update({
         accessKeyId: "woof",
@@ -8,3 +10,5 @@ if (process.env.IS_OFFLINE || process.env.NODE_ENV === "test") {
     });
     dynamoose.local();
 }
+
+process.env.FLICKR_API_KEY = "FLICKR_API_KEY";
