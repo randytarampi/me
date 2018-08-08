randytarampi/me

commit 177a9d958f64d77e9541e086d5c95c2285c0c52b

feat(jsx): Handle the HTML returned in tumblr photo captions.
Just hack at the `<p>` tags and insert `<span>`s into them. A hack sure, but it's definitely an improvement. Feel bad about no tests and committing directly on master though, but I made #12 to shame me for next time.

Closes randytarampi/me.photos#17. Finally. Lol.

packages/jsx/lib/components/post.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import React, {Component} from "react";
 import Dimensions from "react-dimensions";
 import {Col, Row} from "react-materialize";
+import isHtml from "is-html";
 
 export class PostComponent extends Component {
     get width() {
@@ -68,18 +69,29 @@ export class PostComponent extends Component {
                     }
                 </p>
                 {
-                    typeof this.props.post.body === "string" ?
-                        <div className="post-body" dangerouslySetInnerHTML={{__html: this.props.post.body}}/> :
+                    typeof this.props.post.body === "string"
+                        ? <div className="post-body">
+                            {
+                                isHtml(this.props.post.body)
+                                    ? <div dangerouslySetInnerHTML={{__html: this.props.post.body}}></div>
+                                    : <span className="post-text" dangerouslySetInnerHTML={{__html: this.props.post.body}}/>
+                            }
+                        </div> :
                         null
                 }
                 {
-                    Array.isArray(this.props.post.body) ?
-                        this.props.post.body.map((htmlString, index) => {
+                    Array.isArray(this.props.post.body)
+                        ? this.props.post.body.map((htmlString, index) => {
                             return <div className="post-body" key={index}>
+                                {
+                                    isHtml(htmlString)
+                                        ? <div dangerouslySetInnerHTML={{__html: htmlString}}></div>
+                                        : <span className="post-text" dangerouslySetInnerHTML={{__html: htmlString}}/>
+                                }
                                 <span className="post-text" dangerouslySetInnerHTML={{__html: htmlString}}/>
                             </div>;
-                        }) :
-                        null
+                        })
+                        : null
                 }
             </Col>
         </Row>;

packages/jsx/lib/components/photo.jsx
@@ -5,6 +5,7 @@ import React, {Fragment} from "react";
 import Dimensions from "react-dimensions";
 import {Col, Row} from "react-materialize";
 import {PostComponent} from "./post";
+import isHtml from "is-html";
 
 export class PhotoComponent extends PostComponent {
     get width() {
@@ -55,7 +56,11 @@ export class PhotoComponent extends PostComponent {
                 {
                     typeof this.props.post.body === "string" && this.props.post.body !== "" ?
                         <p className="photo-body">
-                            <span className="photo-text" dangerouslySetInnerHTML={{__html: this.props.post.body}}/>
+                            {
+                                isHtml(this.props.post.body)
+                                    ? <div dangerouslySetInnerHTML={{__html: this.props.post.body}}></div>
+                                    : <span className="photo-text" dangerouslySetInnerHTML={{__html: this.props.post.body}}/>
+                            }
                         </p> :
                         null
                 }
@@ -64,7 +69,11 @@ export class PhotoComponent extends PostComponent {
                         this.props.post.body.map((htmlString, index) => {
                             return <p className="photo-body"
                                       key={`${this.props.post.id}:${this.props.post.type}:body:${index}`}>
-                                <span className="photo-text" dangerouslySetInnerHTML={{__html: htmlString}}/>
+                                    {
+                                        isHtml(htmlString)
+                                            ? <div dangerouslySetInnerHTML={{__html: htmlString}}></div>
+                                            : <span className="photo-text" dangerouslySetInnerHTML={{__html: htmlString}}/>
+                                    }
                             </p>;
                         }) :
                         null

packages/posts/photos/tumblr/photoSource.js
@@ -57,7 +57,7 @@ class TumblrSource extends PhotoSource {
             sizedPhotos,
             postJson.post_url,
             null,
-            photoJson.caption || postJson.caption,
+            processCaptionHtml(photoJson.caption || postJson.caption),
             new Creator(
                 blogJson.name,
                 blogJson.name,
@@ -67,5 +67,12 @@ class TumblrSource extends PhotoSource {
         );
     }
 }
+
+// # NOTE-RT: This is pretty gross modifying the HTML we're passing along, but I think this'll be more common than not for other post providers going forwards, especially the more sophisticated platforms.
+export const processCaptionHtml = caption => {
+    return caption
+        .replace(/<p>/g, "<p><span class=\"photo-text\">")
+        .replace(/<\/p>/g, "</span></p>");
+};
 
 export default TumblrSource;

commit 0d10271de91dcb207b474d7bf7c2d8a608c7f472

fix(jsx): Don't duplicate `Post.body` text when `Post.body` is an `Array`.
Regression from 177a9d958f64d77e9541e086d5c95c2285c0c52b.

packages/jsx/lib/components/post.jsx
@@ -1,10 +1,10 @@
 import {Post as PostEntity} from "@randy.tarampi/js";
-import Link from "./link";
+import isHtml from "is-html";
 import PropTypes from "prop-types";
 import React, {Component} from "react";
 import Dimensions from "react-dimensions";
 import {Col, Row} from "react-materialize";
-import isHtml from "is-html";
+import Link from "./link";
 
 export class PostComponent extends Component {
     get width() {
@@ -88,7 +88,6 @@ export class PostComponent extends Component {
                                         ? <div dangerouslySetInnerHTML={{__html: htmlString}}></div>
                                         : <span className="post-text" dangerouslySetInnerHTML={{__html: htmlString}}/>
                                 }
-                                <span className="post-text" dangerouslySetInnerHTML={{__html: htmlString}}/>
                             </div>;
                         })
                         : null