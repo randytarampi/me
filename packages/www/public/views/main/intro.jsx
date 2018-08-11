f68225fad0bab973976ae65db28918128cdf0eb3
Randy Tarampi <rt@randytarampi.ca>
2018-08-11 12:13:55 -0700

    fix(www): Fix `Intro` styles.
    Yikes. I definitely need to look over those pages when I'm updating my resume styles.

--- packages/www/public/views/main/intro.jsx
+++ packages/www/public/views/main/intro.jsx
@@ -8,36 +8,54 @@ import Link, {
     LinkedInLink,
     TelLink
 } from "@randy.tarampi/jsx/lib/components/link";
-import React from "react";
+import React, {Fragment} from "react";
 import {Col} from "react-materialize";
 
-const Intro = () =>
-    <RowBlock name="intro">
-        <Col className="block__bear" l={5} m={8} s={12} offset="m2">
-            <HelloBear/>
-        </Col>
-        <Col className="block__text" l={7} m={8} s={12} offset="m2">
-            <h2>
-                <span className="text">Hey!</span>
-            </h2>
-            <p>
+const IntroText = () =>
+    <Fragment>
+        <h2>
+            <span className="text">Hey!</span>
+        </h2>
+        <p>
                 <span className="text">I'm looking for work, but you probably knew that already because let's be real, you probably clicked through to here from my <Link
                     href="/resume">resume</Link> or my <LinkedInLink useBranding={false} text="LinkedIn"/>.</span>
-            </p>
-            <p>
+        </p>
+        <p>
                 <span className="text">If you're interested in my work check me out on <GitHubLink useBranding={false}
                                                                                                    text="GitHub"/>, look at where I've been on <InstagramLink
                     useBranding={false} text="Instagram"/> or peek in on what's going on in my life at my <BlogAppLink
                     text="blog"/>.</span>
-            </p>
-            <p>
+        </p>
+        <p>
                 <span className="text">
                     And if you're still interested after all that, shoot me an <EmailLink useBranding={false}
                                                                                           text="email"/> or <TelLink
                     useBranding={false} text="text"/> and let's have a chat!
                 </span>
-            </p>
-        </Col>
-    </RowBlock>;
+        </p>
+    </Fragment>;
+
+const Intro = () =>
+    <Fragment>
+        <RowBlock name="intro" className="intro--large hide-on-med-and-down">
+            <Col className="block__bear" l={5} m={8} s={12} offset="m2">
+                <HelloBear/>
+            </Col>
+            <Col className="block__text" l={7}>
+                <IntroText/>
+            </Col>
+        </RowBlock>
+        <RowBlock name="intro-responsive-bear" className="intro-responsive-bear hide-on-large-only">
+            <Col className="block__bear" l={5} m={8} s={12} offset="m2">
+                <HelloBear/>
+            </Col>
+        </RowBlock>
+        <RowBlock name="intro-responsive-text" className="intro-responsive-text hide-on-large-only">
+            <Col className="block__text" m={8} s={12} offset="m2">
+                <IntroText/>
+            </Col>
+        </RowBlock>
+    </Fragment>;
+
 
 export default Intro;
