b86f8e238501bd1320538e1dcb8e45ba27f38ec8
Randy Tarampi <rt@randytarampi.ca>
2018-08-15 00:39:16 -0700

    fix(posts): `me.service.client` throws when it receives unexpected responses.
    Tests to follow when I can remember the best way of mocking modules that just export functions. Can't even introspect `isomorphic-fetch` because it just delegates to `node-fetch` which also exports just a function.
    
    Address #39

--- packages/posts/blog/me.service.client.js
+++ packages/posts/blog/me.service.client.js
@@ -2,8 +2,16 @@ import fetch from "isomorphic-fetch";
 import logger from "../lib/logger";
 
 export default (fetchUrl, page) => {
-    return fetch(`${fetchUrl}?page=${page}`)
+    const url = `${fetchUrl}?page=${page}`;
+    return fetch(url)
         .then(body => body.json())
+        .then(posts => {
+            if (!posts || !(posts instanceof Array)) {
+                throw new Error(`Unexpected non-error output ${JSON.stringify(posts)} from ${url}`);
+            }
+
+            return posts;
+        })
         .catch(error => {
             logger.error(error);
             return [];
