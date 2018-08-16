467d14244a4f0effc8f4636a6c51159a6757936b
Randy Tarampi <rt@randytarampi.ca>
2018-08-16 09:38:22 -0700

    fix(resume): Need to use express to serve static assets for puppeteer.
    An easy to implement suggestion from https://github.com/GoogleChrome/puppeteer/issues/1643#issuecomment-353387148.

---
+++ packages/resume/express.js
@@ -0,0 +1,10 @@
+require("../../babel.register.js");
+
+const path = require("path");
+process.env.NODE_CONFIG_DIR = path.join(__dirname, "../../config");
+
+const express = require("express");
+const app = express();
+
+app.use(express.static(`${__dirname}/dist`));
+module.exports = app.listen(3000);
