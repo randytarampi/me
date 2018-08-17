1646acefd44401e2ffb1e85362d478839a93edaa
Randy Tarampi <randy.tarampi@gmail.com>
2018-08-17 13:53:09 -0700

    feat(posts): Use `serverless-plugin-warmup` for the `get*` lambdas.
    Response time~~.

--- packages/posts/serverless/handler/getPhotos.js
+++ packages/posts/serverless/handler/getPhotos.js
@@ -5,6 +5,10 @@ import responseBuilder from "../util/responseBuilder";
 import returnErrorResponse from "../util/returnErrorResponse";
 
 export default (event, context, callback) => {
+    if (event.source === "serverless-plugin-warmup") {
+        return callback(null, "Lambda is warm!");
+    }
+
     configureEnvironment()
         .then(() => {
             const searchParams = new SearchParams({
--- packages/posts/serverless/handler/getWords.js
+++ packages/posts/serverless/handler/getWords.js
@@ -4,6 +4,10 @@ import responseBuilder from "../util/responseBuilder";
 import returnErrorResponse from "../util/returnErrorResponse";
 
 export default (event, context, callback) => {
+    if (event.source === "serverless-plugin-warmup") {
+        return callback(null, "Lambda is warm!");
+    }
+    
     configureEnvironment()
         .then(() => {
             return searchWords(event.queryStringParameters)
