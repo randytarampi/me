508cff4ff196bb817cd8d98ee1cb70a3a6dea3ef
Randy Tarampi <rt@randytarampi.ca>
2018-08-19 13:55:48 -0700

    feat(sentry): Upload sentry artifacts for `dev` builds.
    Might as well test *everything*.

--- packages/letter/webpack.config.js
+++ packages/letter/webpack.config.js
@@ -35,13 +35,13 @@ const plugins = [
     })
 ];
 
-if (process.env.TRAVIS_TAG && process.env.SENTRY_AUTH_TOKEN) {
+if (process.env.DEPLOY && process.env.SENTRY_AUTH_TOKEN) {
     plugins.push(
         new SentryPlugin({
             organization: process.env.SENTRY_ORG,
             project: process.env.SENTRY_PROJECT,
             apiKey: process.env.SENTRY_AUTH_TOKEN,
-            release: process.env.TRAVIS_TAG,
+            release: process.env.TRAVIS_TAG || process.env.TRAVIS_COMMIT,
             releaseBody: (version, projects) => {
                 return {
                     version,
