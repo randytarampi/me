randytarampi/me

commit 4876717388754113e12cbfa4f1239b77e54601cd

fix(jsx): Per 3ddb39d855a34a9b756091c6b90286465ab2db89, fix `.loading-spinner` centering.

packages/jsx/lib/components/posts.jsx
@@ -13,8 +13,9 @@ export class PostsComponent extends Component {
             useWindowAsScrollContainer={true}
             elementHeight={this.props.posts ? this.props.posts.map(computePostHeight(this.props.containerWidth)) : [500]}
             infiniteLoadBeginEdgeOffset={Infinite.containerHeightScaleFactor(0.05).amount}
-            onInfiniteLoad={this.props.fetchPosts}
-            isInfiniteLoading={this.props.isLoading}
+            onInfiniteLoad={() => {
+            }}
+            isInfiniteLoading={true}
             loadingSpinnerDelegate={<LoadingSpinner/>}
         >
             {

packages/css/styles/spinner.scss
@@ -1,10 +1,6 @@
 .loading-spinner {
     min-height: 100vh;
-}
-
-.preloader-wrapper {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-right: -50%;
+    display: flex;
+    align-items: center;
+    justify-content: center
 }

commit e067f2d2cd873e937b03b43a8f9460fae9d26933

fix(jsx): Fix `.loading-spinner` regression introduced in 4876717388754113e12cbfa4f1239b77e54601cd.
Stubbed those out to facilitate testing. I guess my self code-reviewing is paying off?

packages/jsx/lib/components/posts.jsx
@@ -13,9 +13,8 @@ export class PostsComponent extends Component {
             useWindowAsScrollContainer={true}
             elementHeight={this.props.posts ? this.props.posts.map(computePostHeight(this.props.containerWidth)) : [500]}
             infiniteLoadBeginEdgeOffset={Infinite.containerHeightScaleFactor(0.05).amount}
-            onInfiniteLoad={() => {
-            }}
-            isInfiniteLoading={true}
+            onInfiniteLoad={this.props.fetchPosts}
+            isInfiniteLoading={this.props.isLoading}
             loadingSpinnerDelegate={<LoadingSpinner/>}
         >
             {