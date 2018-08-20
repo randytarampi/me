b7c43f1bd704084119097e105de84c4427e549da
Randy Tarampi <rt@randytarampi.ca>
2018-08-19 17:13:20 -0700

    fix(posts): `getPosts` actually returns the `limit`ed number of `Post`s.
    To support randytarampi/me.photos#15.

--- packages/posts/serverless/handler/getPosts.js
+++ packages/posts/serverless/handler/getPosts.js
@@ -1,10 +1,10 @@
-import searchCache from "../../lib/searchCache";
-import configureEnvironment from "../util/configureEnvironment";
-import responseBuilder from "../util/responseBuilder";
-import returnErrorResponse from "../util/returnErrorResponse";
-import parseQueryStringParametersIntoSearchParams from "../util/parseQueryStringParametersIntoSearchParams";
 import {Photo, Post} from "@randy.tarampi/js";
 import _ from "lodash";
+import searchCache from "../../lib/searchCache";
+import configureEnvironment from "../util/configureEnvironment";
+import parseQueryStringParametersIntoSearchParams from "../util/parseQueryStringParametersIntoSearchParams";
+import responseBuilder from "../util/responseBuilder";
+import returnErrorResponse from "../util/returnErrorResponse";
 
 export default (event, context, callback) => {
     if (event.source === "serverless-plugin-warmup") {
@@ -18,11 +18,10 @@ export default (event, context, callback) => {
                     searchCache(parseQueryStringParametersIntoSearchParams({type: Post.name})(event.queryStringParameters))
                 ])
                 .then(_.flatten)
-                .then(flattenedPosts => {
-                    return _.sortBy(flattenedPosts, [
-                        post => -1 * (post.dateCreated ? post.dateCreated.valueOf() : post.datePublished ? post.datePublished.valueOf() : 0)
-                    ]);
-                })
+                .then(flattenedPosts => _.sortBy(flattenedPosts, [
+                    post => -1 * (post.dateCreated ? post.dateCreated.valueOf() : post.datePublished ? post.datePublished.valueOf() : 0)
+                ]))
+                .then(sortedPosts => _.slice(sortedPosts, 0, event.queryStringParameters && event.queryStringParameters.limit || 100))
                 .then(sortedPosts => callback(null, responseBuilder(sortedPosts)));
         })
         .catch(returnErrorResponse(callback));
