46d1db8909a1a1dd24cce13ea1fafad2e5495d8f
Randy Tarampi <rt@randytarampi.ca>
2018-08-20 10:41:54 -0700

    feat(bin): Pull out the `clean` script into an actual shell script.

---
+++ bin/clean.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e;
+
+REPO_ROOT=${TRAVIS_BUILD_DIR:=`pwd`}
+
+cd $REPO_ROOT;
+
+rm -rf packages/*/package-lock.json packages/*/dist packages/*/coverage packages/*/.nyc_output packages/*/.serverless packages/*/.webpack;
+
+npx lerna clean --yes;
+
+rm -rf node_modules package-lock.json coverage .nyc_output;
