b19e81d454646535122d7d3b1e65727932c9ed6c
Randy Tarampi <rt@randytarampi.ca>
2018-08-23 15:50:42 -0700

    fix(resume): Give the Volunteering section a description.

--- packages/resume/public/components/resume/content/volunteer/index.jsx
+++ packages/resume/public/components/resume/content/volunteer/index.jsx
@@ -1,10 +1,22 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, {Fragment} from "react";
 import ResumeSection from "../section";
 import ResumeVolunteerEntry from "./entry";
 
 export const ResumeVolunteer = ({resume}) => {
-    return <ResumeSection type="volunteer" label="Volunteering" hideOnPrint={true}>
+    return <ResumeSection
+        type="volunteer"
+        label="Volunteering"
+        hideOnPrint={true}
+        descriptionNode={
+            <Fragment>
+                <p><span className="text">I never really <em>want</em> to volunteer, but when it happens it seems that I really enjoy it</span>
+                </p>
+                <p><span className="text">People always looked at me as a shy and reserved, but it actually turns out that I've got a knack for leadership and mentorship</span>
+                </p>
+            </Fragment>
+        }
+    >
         {
             resume.volunteer.map((volunteerEntry, index) => {
                 return <ResumeVolunteerEntry volunteerEntry={volunteerEntry} key={index} index={index}/>;
