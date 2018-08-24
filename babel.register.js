bcde2ad06f468b94461799a821ada92666ba65ec
Randy Tarampi <rt@randytarampi.ca>
2018-08-23 17:46:45 -0700

    fix: Also `@babel/register` query-string, strip-ansi, ansi-regex, etc.
    Per 41ec9b4ecb4c3dc64c5a9795316d1200a73ae6ef.

--- babel.register.js
+++ babel.register.js
@@ -3,6 +3,6 @@ const path = require("path");
 require("@babel/register")({
     configFile: path.join(__dirname, "babel.config.js"),
     only: [
-        /(?:node_modules\/@randy\.tarampi|me\/packages)\//
+        /(?:node_modules\/(@randy\.tarampi|query-string|strict-uri-encode|strip-ansi|ansi-regex)|me\/packages)\//
     ]
 });
