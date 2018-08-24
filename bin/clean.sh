749097e2a31fad64698853f36c057bff6d621699
Randy Tarampi <rt@randytarampi.ca>
2018-08-24 15:12:05 -0700

    fix(posts): Update the `dynamodb` install directory for `dynamodb-localhost`.
    Per fb822559f511378c9600f20c67dd81c8ccf40368.

--- bin/clean.sh
+++ bin/clean.sh
@@ -6,7 +6,7 @@ REPO_ROOT=${TRAVIS_BUILD_DIR:=`pwd`}
 
 cd $REPO_ROOT;
 
-rm -rf packages/*/package-lock.json packages/*/dist packages/*/coverage packages/*/.nyc_output packages/*/.serverless packages/*/.webpack;
+rm -rf packages/*/package-lock.json packages/*/dist packages/*/coverage packages/*/.nyc_output packages/*/.serverless packages/*/.webpack packages/*/.dynamodb;
 
 npx lerna clean --yes;
 
