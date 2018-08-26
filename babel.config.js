randytarampi/me

commit fa27adc5979eb8f9194a6e0e69becf7be909808e

fix: Prefer babel's `minify-replace` over webpack's `DefinePlugin`.
We're loading everything with babel anyways, so why not?

babel.config.js
@@ -128,9 +128,7 @@ module.exports = (api) => {
         [
             "@babel/preset-env",
             {
-                targets: {
-                    node: "current"
-                },
+                forceAllTransforms: true,
                 useBuiltIns: "entry",
                 shippedProposals: true
             }
@@ -146,12 +144,12 @@ module.exports = (api) => {
     let plugins = [
         "@babel/plugin-proposal-object-rest-spread",
         "react-hot-loader/babel",
-        "lodash"
+        "lodash",
+        configuredMinifyReplace
     ];
 
     switch (api.env()) {
         case "test": {
-            plugins.push(configuredMinifyReplace);
             plugins.push("istanbul");
             break;
         }
@@ -173,26 +171,9 @@ module.exports = (api) => {
             break;
         }
 
-        case "client": {
-            presets = [
-                [
-                    "@babel/preset-env",
-                    {
-                        forceAllTransforms: true,
-                        useBuiltIns: "entry",
-                        shippedProposals: true
-                    }
-                ],
-                [
-                    "@babel/preset-react"
-                ]
-            ];
-            break;
-        }
-
+        case "client":
         case "development":
         default: {
-            plugins.push(configuredMinifyReplace);
             break;
         }
     }

webpack.client.config.base.js
@@ -7,8 +7,7 @@ const SentryPlugin = require("webpack-sentry-plugin");
 const util = require("./util");
 
 const isDevelopment = process.env.WEBPACK_SERVE
-    || process.env.NODE_ENV !== "production"
-    || process.env.NODE_ENV !== "prd"
+    || !["production", "prd"].includes(process.env.NODE_ENV)
     || true;
 
 const resolveMode = () => {
@@ -19,25 +18,7 @@ const resolveMode = () => {
     return "production";
 };
 
-const plugins = [
-    new webpack.DefinePlugin({
-        __WORDS_SERVICE_URL__: JSON.stringify(config.get("wordsServiceUrl")),
-        __POSTS_SERVICE_URL__: JSON.stringify(config.get("postsServiceUrl")),
-        __PHOTOS_SERVICE_URL__: JSON.stringify(config.get("photosServiceUrl")),
-        __CODE_APP_URL__: JSON.stringify(config.get("codeAppUrl")),
-        __WORDS_APP_URL__: JSON.stringify(config.get("wordsAppUrl")),
-        __POSTS_APP_URL__: JSON.stringify(config.get("postsAppUrl")),
-        __PHOTOS_APP_URL__: JSON.stringify(config.get("photosAppUrl")),
-        __RESUME_APP_URL__: JSON.stringify(config.get("resumeAppUrl")),
-        __ASSET_URL__: JSON.stringify(config.get("assetUrl")),
-        __PUBLISHED_RESUME_URL__: JSON.stringify(config.get("resume.publishUrl")),
-        __CAMPAIGN_SOURCE__: JSON.stringify(config.get("me.campaign.source")),
-        __CAMPAIGN_MEDIUM__: JSON.stringify(config.get("me.campaign.medium")),
-        __CAMPAIGN_NAME__: JSON.stringify(config.get("me.campaign.name")),
-        __CAMPAIGN_TERM__: JSON.stringify(config.get("me.campaign.term")),
-        __CAMPAIGN_CONTENT__: JSON.stringify(config.get("me.campaign.content"))
-    })
-];
+const plugins = [];
 
 if (process.env.DEPLOY && process.env.SENTRY_AUTH_TOKEN) {
     plugins.push(

commit 6f3f86561f687ba45df774a6031b80fda972a921

fix: Fix babel compilation error introduced in fa27adc5979eb8f9194a6e0e69becf7be909808e.

babel.config.js
@@ -128,7 +128,9 @@ module.exports = (api) => {
         [
             "@babel/preset-env",
             {
-                forceAllTransforms: true,
+                targets: {
+                    node: "current"
+                },
                 useBuiltIns: "entry",
                 shippedProposals: true
             }
@@ -171,7 +173,26 @@ module.exports = (api) => {
             break;
         }
 
-        case "client":
+        case "client": {
+            presets = [
+                [
+                    "@babel/preset-env",
+                    {
+                        forceAllTransforms: true,
+                        useBuiltIns: "entry",
+                        shippedProposals: true
+                    }
+                ],
+                [
+                    "@babel/preset-react",
+                    {
+                        development: api.env() !== "prd"
+                    }
+                ]
+            ];
+            break;
+        }
+
         case "development":
         default: {
             break;