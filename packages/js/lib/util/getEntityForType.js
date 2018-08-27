4929399324197924b18f52085043607c8365a84f
Randy Tarampi <rt@randytarampi.ca>
2018-08-27 11:43:16 -0700

    fix(js): `getEntityForType` shouldn't rely on class names in the switch/case.
    Webpack's discarding them or something? Add some logging to figure it out – getting `Error: Can't `getEntityForType` for `Post``...

--- packages/js/lib/util/getEntityForType.js
+++ packages/js/lib/util/getEntityForType.js
@@ -4,9 +4,13 @@ import Post from "../post";
 export default type => {
     switch (type) {
         case Photo.name:
+        case "Photo":
+            console.log("Photo.name", Photo.name); // eslint-disable-line no-console
             return Photo;
 
         case Post.name:
+        case "Post":
+            console.log("Post.name", Post.name); // eslint-disable-line no-console
             return Post;
 
         default:
