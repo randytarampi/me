4c62e5c105faba7c05f67625f17938050418c138
Randy Tarampi <rt@randytarampi.ca>
2018-08-27 12:27:58 -0700

    fix(js): Remove logging from 4929399324197924b18f52085043607c8365a84f.
    The minifier seems to be turning `Post.name` into `t`. Don't quite know how it's not doing the same thing for `Photo`, but just leave the hard coded hack in there for now.

--- packages/js/lib/util/getEntityForType.js
+++ packages/js/lib/util/getEntityForType.js
@@ -5,12 +5,10 @@ export default type => {
     switch (type) {
         case Photo.name:
         case "Photo":
-            console.log("Photo.name", Photo.name); // eslint-disable-line no-console
             return Photo;
 
         case Post.name:
         case "Post":
-            console.log("Post.name", Post.name); // eslint-disable-line no-console
             return Post;
 
         default:
