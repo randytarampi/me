fbd50d781c5845c9736f985f979bc05fb7cbe0c9
Randy Tarampi <rt@randytarampi.ca>
2018-08-28 10:18:58 -0700

    fix(jsx): JSX tests also define a JSDOM `global.document`.
    From my changes in 10a967ac798ad8a072b894cfeee61fedfbe22091.

--- packages/jsx/test/01_setup.js
+++ packages/jsx/test/01_setup.js
@@ -8,6 +8,7 @@ import {JSDOM} from "jsdom";
 
 const jsdom = new JSDOM();
 global.window = jsdom.window;
+global.document = jsdom.window.document;
 global.navigator = {
     userAgent: "node.js"
 };
