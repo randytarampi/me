f13cdca71ce87bd1407acb18717eac5cf63019e5
Randy Tarampi <rt@randytarampi.ca>
2018-08-30 11:24:15 -0700

    fix(jsx): Work around jankiness loading `Post`s.
    Ugh. This `react-infinite`/`react-dimensions` combination just isn't working out. I'll take up #70. `Infinite` doesn't seem to be able to keep track of the scroll position properly.
    
    Let's try to avoid having to render the `LoadingSpinner` on the page – I think the `100vh` `min-height` on that might be tricking `Infinite`'s scroll calculation.

--- packages/jsx/lib/components/posts.jsx
+++ packages/jsx/lib/components/posts.jsx
@@ -7,29 +7,30 @@ import LoadingSpinner from "../components/loadingSpinner";
 import computePostHeight from "../util/computePostHeight";
 import getComponentForType from "../util/getComponentForType";
 
-export class PostsComponent extends Component {
-    render() {
-        return <Infinite
-            useWindowAsScrollContainer={true}
-            elementHeight={this.props.posts ? this.props.posts.toArray().map(computePostHeight(this.props.containerWidth)) : [500]}
-            infiniteLoadBeginEdgeOffset={50}
-            preloadBatchSize={Infinite.containerHeightScaleFactor(0.5)}
-            preloadAdditionalHeight={Infinite.containerHeightScaleFactor(2)}
-            onInfiniteLoad={this.props.fetchPosts}
-            isInfiniteLoading={this.props.isLoading}
-            loadingSpinnerDelegate={<LoadingSpinner/>}
-        >
-            {
-                this.props.posts
-                    ? this.props.posts.toArray().map(post => {
-                        const Constructor = getComponentForType(post.type);
-                        return <Constructor key={post.uid} post={post}/>;
-                    })
-                    : <div/>
-            }
-        </Infinite>;
-    }
-}
+const PostsComponent = props => {
+    const postsArray = props.posts && props.posts.toArray();
+
+    return <Infinite
+        containerWidth={props.containerWidth}
+        useWindowAsScrollContainer={true}
+        elementHeight={postsArray ? postsArray.map(computePostHeight(props.containerWidth)) : [0]}
+        infiniteLoadBeginEdgeOffset={500}
+        preloadBatchSize={Infinite.containerHeightScaleFactor(0.5)}
+        preloadAdditionalHeight={Infinite.containerHeightScaleFactor(2)}
+        onInfiniteLoad={props.fetchPosts}
+        isInfiniteLoading={props.isLoading}
+        loadingSpinnerDelegate={<LoadingSpinner/>}
+    >
+        {
+            postsArray
+                ? postsArray.map(post => {
+                    const Constructor = getComponentForType(post.type);
+                    return <Constructor key={post.uid} post={post}/>;
+                })
+                : <div/>
+        }
+    </Infinite>;
+};
 
 PostsComponent.propTypes = {
     containerHeight: PropTypes.number,
