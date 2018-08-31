a8920be7d3c875e2220a351e665f45516de9c830
Randy Tarampi <rt@randytarampi.ca>
2018-08-31 14:04:43 -0700

    chore(posts): Remove the `getWords` and `getPhotos` handlers.

--- packages/posts/test/02_import-all.js
+++ packages/posts/test/02_import-all.js
@@ -1,6 +1,4 @@
 import "../serverless/handlers/cachePosts/initialized";
-import "../serverless/handlers/getPhotos/initialized";
 import "../serverless/handlers/getPosts/initialized";
-import "../serverless/handlers/getWords/initialized";
 import "../serverless/handlers/instagramAuthRedirect/initialized";
 import "../serverless/handlers/instagramAuthReturn/initialized";
