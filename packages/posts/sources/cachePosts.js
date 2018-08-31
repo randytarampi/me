30dd336889e03f7c8405fdf95cdcc29824bf08a7
Randy Tarampi <rt@randytarampi.ca>
2018-08-31 15:14:04 -0700

    fix(posts): `initializedSources` is actually a Promised array.

--- packages/posts/sources/cachePosts.js
+++ packages/posts/sources/cachePosts.js
@@ -2,13 +2,14 @@ import logger from "../lib/logger";
 import initializedSources from "./";
 
 const cachePosts = searchParams => {
-    return Promise.all(initializedSources.map(postSource => {
-        return postSource.getAllServicePosts(searchParams)
-            .catch(error => {
-                logger.error(error);
-                return [];
-            });
-    }));
+    return initializedSources
+        .then(initializedSources => Promise.all(initializedSources.map(postSource => {
+            return postSource.getAllServicePosts(searchParams)
+                .catch(error => {
+                    logger.error(error);
+                    return [];
+                });
+        })));
 };
 
 export default cachePosts;
