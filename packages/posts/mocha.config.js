8a8d5e91c80ab291ac23268de750b417d94e89f2
Randy Tarampi <rt@randytarampi.ca>
2018-08-31 15:05:25 -0700

    chore: `cover` script shouldn't generate coverage for `cobertura`.
    Also, I can kinda understand why `@randy.tarampi/js` would cause problems for `nyc`'s instrumentation. Kinda. It's just bizzare that because the folder is named `js` it wouldn't be able to parse it.

--- packages/posts/mocha.config.js
+++ packages/posts/mocha.config.js
@@ -3,6 +3,6 @@ module.exports = {
     sort: true,
     fullTrace: true,
     checkLeaks: true,
-    require: ["../../babel.register.js", "./test/01_setup", "./test/02_import-all"],
+    require: ["../../babel.register.js", "test/01_setup", "test/02_import-all"],
     exit: true
 };
