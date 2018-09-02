eb6f4c0ae57f864489694ebbde1cecfa3daf2760
Randy Tarampi <rt@randytarampi.ca>
2018-09-02 15:53:44 -0700

    chore(jsx): Log `ERROR`s when we `dispatch(setError(error, ...))`.
    And log `WARN` when we don't actually dispatch `setError` with an `error`.

--- packages/jsx/lib/actions/setError.js
+++ packages/jsx/lib/actions/setError.js
@@ -1,8 +1,14 @@
 import {createAction} from "redux-actions";
+import logger from "../logger";
 
 export const SET_ERROR = "SET_ERROR";
 
 export default (error, errorCode, errorMessage) => dispatch => {
+    if (error) {
+        logger.error(error);
+    } else {
+        logger.warn(errorCode, errorMessage);
+    }
     dispatch(setError({error, errorCode, errorMessage}));
 };
 
