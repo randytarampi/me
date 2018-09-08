randytarampi/me

commit d7ae34af33dfd9bcfe75888ee5035590ec84e6c7

chore(resume): Add some test coverage and close #89.

packages/resume/public/components/resume/content/education/entry.jsx
@@ -23,8 +23,7 @@ export const ResumeEducationEntry = ({educationEntry, index}) => {
                             <span className="text">
                             {
                                 educationEntry.website
-                                    ? <CampaignLink
-                                        href={educationEntry.website}>{educationEntry.institution}</CampaignLink>
+                                    ? <CampaignLink href={educationEntry.website} text={educationEntry.institution}/>
                                     : educationEntry.institution
                             }
                             </span>
@@ -60,7 +59,9 @@ export const ResumeEducationEntry = ({educationEntry, index}) => {
                             <ul className="resume-education-entry__highlights">
                                 {
                                     educationEntry.courses.map((highlight, index) => {
-                                        return <li className={`resume-education-entry__highlight${index > 3 ? " hide-on-print" : ""}`} key={index}>
+                                        return <li
+                                            className={`resume-education-entry__highlight${index < 3 ? " show-on-letter show-on-a4" : ""}${index >= 3 ? " show-on-legal" : ""}`}
+                                            key={index}>
                                             {highlight}
                                         </li>;
                                     })

packages/resume/test/unit/public/components/resume/content/education/entry.jsx
@@ -1,0 +1,94 @@
+import React from "react";
+import {expect} from "chai";
+import {shallow} from "enzyme";
+import ResumeEducationEntry from "../../../../../../../public/components/resume/content/education/entry";
+import {CampaignLink} from "@randy.tarampi/jsx";
+
+describe("ResumeEducationEntry", function () {
+    let stubResumeEducationEntry;
+
+    beforeEach(function () {
+        stubResumeEducationEntry = {
+            institution: "University",
+            area: "Woofing and Meowing",
+            studyType: "Bachelor",
+            startDate: "2011-01-01",
+            endDate: "2013-01-01",
+            gpa: "4.33",
+            courses: [
+                "Woofing",
+                "Meowing",
+                "Grring",
+                "Rawring",
+                "Yelling",
+                "Quietly speaking when asked",
+            ],
+            website: "woof://woof.woof/woof"
+        };
+    });
+
+    it("renders", function () {
+        const rendered = shallow(<ResumeEducationEntry educationEntry={stubResumeEducationEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+        expect(rendered).to.not.have.className("hide-on-print");
+        expect(rendered).to.have.descendants(".resume-education-entry");
+        expect(rendered).to.have.descendants(".resume-education-entry__basics");
+        expect(rendered).to.have.descendants(".resume-education-entry__date");
+        expect(rendered).to.have.descendants(".resume-education-entry__institution");
+        expect(rendered.find(CampaignLink)).to.be.ok;
+        expect(rendered.find(CampaignLink)).to.have.length(1);
+        expect(rendered.find(CampaignLink)).to.have.prop("href", stubResumeEducationEntry.website);
+        expect(rendered.find(CampaignLink)).to.have.prop("text", stubResumeEducationEntry.institution);
+
+        expect(rendered).to.have.descendants(".resume-education-entry__details");
+        expect(rendered).to.have.descendants(".resume-education-entry__details > .right.hide-on-small-only > .resume-education-entry__area");
+        expect(rendered).to.have.descendants(".resume-education-entry__details > .hide-on-med-and-up > .resume-education-entry__area");
+        expect(rendered).to.have.descendants(".resume-education-entry__details > div > .resume-education-entry__study-type");
+        expect(rendered).to.have.descendants(".resume-education-entry__highlights");
+        expect(rendered).to.have.descendants(".resume-education-entry__highlight");
+        expect(rendered.find(".resume-education-entry__highlight")).to.have.length(stubResumeEducationEntry.courses.length);
+        expect(rendered.find(".resume-education-entry__highlight.show-on-letter.show-on-a4")).to.have.length(3);
+        expect(rendered.find(".resume-education-entry__highlight.show-on-legal")).to.have.length(stubResumeEducationEntry.courses.length - 3);
+    });
+
+    it("renders (no end date)", function () {
+        delete stubResumeEducationEntry.endDate;
+
+        const rendered = shallow(<ResumeEducationEntry educationEntry={stubResumeEducationEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+        expect(rendered.find(".resume-education-entry__basics > .right.hide-on-small-only > .resume-education-entry__date").html()).to.match(/ to Present/);
+        expect(rendered.find(".resume-education-entry__basics > .hide-on-med-and-up > .resume-education-entry__date").html()).to.match(/ to Present/);
+    });
+
+    it("renders (`.hide-on-print` if 4th or subsequent project)", function () {
+        const rendered = shallow(<ResumeEducationEntry educationEntry={stubResumeEducationEntry} index={4}/>);
+
+        expect(rendered).to.be.ok;
+        expect(rendered).to.have.className("hide-on-print");
+    });
+
+    it("renders (no `educationEntry.website`)", function () {
+        delete stubResumeEducationEntry.website;
+        const rendered = shallow(<ResumeEducationEntry educationEntry={stubResumeEducationEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+
+        expect(rendered.find(CampaignLink)).to.be.ok;
+        expect(rendered.find(CampaignLink)).to.have.length(0);
+        expect(rendered.find(".resume-education-entry__institution")).to.contain(
+            <span className="text">{stubResumeEducationEntry.institution}</span>
+        );
+    });
+
+    it("renders (no `educationEntry.courses`)", function () {
+        delete stubResumeEducationEntry.courses;
+        const rendered = shallow(<ResumeEducationEntry educationEntry={stubResumeEducationEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+
+        expect(rendered).to.not.have.descendants(".resume-education-entry__highlights");
+        expect(rendered).to.not.have.descendants(".resume-education-entry__highlight");
+    });
+});

packages/resume/public/components/resume/content/projects/entry.jsx
@@ -23,8 +23,8 @@ export const ResumeProjectsEntry = ({projectsEntry, index}) => {
                             <span className="text">
                             {
                                 projectsEntry.url
-                                    ? <CampaignLink href={projectsEntry.url}>{projectsEntry.name}</CampaignLink>
-                                    : projectsEntry.company
+                                    ? <CampaignLink href={projectsEntry.url} text={projectsEntry.name}/>
+                                    : projectsEntry.name
                             }
                             </span>
                         </span>

packages/resume/public/components/resume/content/publications/entry.jsx
@@ -20,7 +20,7 @@ export const ResumePublicationsEntry = ({publicationsEntry, index}) => {
                             <span className="text">
                             {
                                 publicationsEntry.url
-                                    ? <CampaignLink href={publicationsEntry.url}>{publicationsEntry.name}</CampaignLink>
+                                    ? <CampaignLink href={publicationsEntry.url} text={publicationsEntry.name}/>
                                     : publicationsEntry.name
                             }
                             </span>

packages/resume/public/components/resume/content/volunteer/entry.jsx
@@ -23,8 +23,7 @@ export const ResumeVolunteerEntry = ({volunteerEntry, index}) => {
                             <span className="text">
                             {
                                 volunteerEntry.website
-                                    ? <CampaignLink
-                                        href={volunteerEntry.website}>{volunteerEntry.organization}</CampaignLink>
+                                    ? <CampaignLink href={volunteerEntry.website} text={volunteerEntry.organization}/>
                                     : volunteerEntry.organization
                             }
                             </span>
@@ -66,7 +65,9 @@ export const ResumeVolunteerEntry = ({volunteerEntry, index}) => {
                             <ul className="resume-volunteer-entry__highlights">
                                 {
                                     volunteerEntry.highlights.map((highlight, index) => {
-                                        return <li className="resume-volunteer-entry__highlight" key={index}>
+                                        return <li
+                                            className={`resume-volunteer-entry__highlight${index < 3 ? " show-on-letter show-on-a4" : ""}${index >= 3 ? " show-on-legal" : ""}`}
+                                            key={index}>
                                             {highlight}
                                         </li>;
                                     })

packages/resume/public/components/resume/content/work/entry.jsx
@@ -23,7 +23,7 @@ export const ResumeWorkEntry = ({workEntry, index}) => {
                             <span className="text">
                             {
                                 workEntry.website
-                                    ? <CampaignLink href={workEntry.website}>{workEntry.company}</CampaignLink>
+                                    ? <CampaignLink href={workEntry.website} text={workEntry.company}/>
                                     : workEntry.company
                             }
                             </span>

packages/resume/test/unit/public/components/resume/content/awards/entry.jsx
@@ -1,0 +1,39 @@
+import React from "react";
+import {expect} from "chai";
+import {shallow} from "enzyme";
+import ResumeAwardsEntry from "../../../../../../../public/components/resume/content/awards/entry";
+
+describe("ResumeAwardsEntry", function () {
+    let stubResumeAwardsEntry;
+
+    beforeEach(function () {
+        stubResumeAwardsEntry = {
+            "title": "Award",
+            "date": "2014-11-01",
+            "awarder": "Company",
+            "summary": "There is no spoon."
+        };
+    });
+
+    it("renders", function () {
+        const rendered = shallow(<ResumeAwardsEntry awardsEntry={stubResumeAwardsEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+        expect(rendered).to.not.have.className("hide-on-print");
+        expect(rendered).to.have.descendants(".resume-awards-entry");
+        expect(rendered).to.have.descendants(".resume-awards-entry__basics");
+        expect(rendered).to.have.descendants(".resume-awards-entry__date");
+        expect(rendered).to.have.descendants(".resume-awards-entry__title");
+        expect(rendered).to.have.descendants(".resume-awards-entry__awarder");
+        expect(rendered).to.have.descendants(".resume-awards-entry__summary");
+        expect(rendered).to.have.descendants(".hide-on-med-and-up > .resume-awards-entry__date");
+        expect(rendered).to.have.descendants(".right.hide-on-small-only > .resume-awards-entry__date");
+    });
+
+    it("renders (`.hide-on-print` if 4th or subsequent project)", function () {
+        const rendered = shallow(<ResumeAwardsEntry awardsEntry={stubResumeAwardsEntry} index={4}/>);
+
+        expect(rendered).to.be.ok;
+        expect(rendered).to.have.className("hide-on-print");
+    });
+});

packages/resume/test/unit/public/components/resume/content/projects/entry.jsx
@@ -1,0 +1,110 @@
+import React from "react";
+import {expect} from "chai";
+import {shallow} from "enzyme";
+import ResumeProjectsEntry from "../../../../../../../public/components/resume/content/projects/entry";
+import {CampaignLink} from "@randy.tarampi/jsx";
+
+describe("ResumeProjectsEntry", function () {
+    let stubResumeProjectsEntry;
+
+    beforeEach(function () {
+        stubResumeProjectsEntry = {
+            name: "My blog",
+            description: "It's a blog",
+            highlights: [
+                "My choice of technologies here was largely driven by my want to minimize costs",
+                "It's all on the AWS Free Tier",
+                "The only thing I'm paying for is DNS",
+                "Code is hosted on GitHub Pages",
+                "CI & CD provided by Travis"
+            ],
+            startDate: "2018-08-01",
+            endDate: "2018-08-01",
+            roles: [
+                "Developer"
+            ],
+            entity: "Me, myself & I",
+            type: "Application",
+            url: "https://www.randytarampi.ca/blog"
+        };
+    });
+
+    it("renders", function () {
+        const rendered = shallow(<ResumeProjectsEntry projectsEntry={stubResumeProjectsEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+        expect(rendered).to.not.have.className("hide-on-print");
+        expect(rendered).to.have.descendants(".resume-projects-entry");
+        expect(rendered).to.have.descendants(".resume-projects-entry__basics");
+        expect(rendered).to.have.descendants(".resume-projects-entry__date");
+        expect(rendered).to.have.descendants(".resume-projects-entry__name");
+        expect(rendered.find(CampaignLink)).to.be.ok;
+        expect(rendered.find(CampaignLink)).to.have.length(2);
+        expect(rendered.find(CampaignLink).first()).to.have.prop("href", stubResumeProjectsEntry.url);
+        expect(rendered.find(CampaignLink).first()).to.have.prop("text", stubResumeProjectsEntry.name);
+
+        expect(rendered).to.have.descendants(".resume-projects-entry__website");
+        expect(rendered.find(CampaignLink).last()).to.have.prop("href", stubResumeProjectsEntry.url);
+        expect(rendered.find(CampaignLink).last()).to.not.have.prop("text");
+
+        expect(rendered).to.have.descendants(".resume-projects-entry__details");
+        expect(rendered).to.have.descendants(".resume-projects-entry__details > div > .resume-projects-entry__position");
+        expect(rendered).to.have.descendants(".resume-projects-entry__description");
+        expect(rendered).to.have.descendants(".resume-projects-entry__highlights");
+        expect(rendered).to.have.descendants(".resume-projects-entry__highlight");
+        expect(rendered.find(".resume-projects-entry__highlight")).to.have.length(stubResumeProjectsEntry.highlights.length);
+        expect(rendered.find(".resume-projects-entry__highlight.show-on-letter.show-on-a4")).to.have.length(3);
+        expect(rendered.find(".resume-projects-entry__highlight.show-on-legal")).to.have.length(stubResumeProjectsEntry.highlights.length - 3);
+    });
+
+    it("renders (no end date)", function () {
+        delete stubResumeProjectsEntry.endDate;
+
+        const rendered = shallow(<ResumeProjectsEntry projectsEntry={stubResumeProjectsEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+        expect(rendered.find(".resume-projects-entry__basics > .right.hide-on-small-only > .resume-projects-entry__date").html()).to.match(/ to Present/);
+        expect(rendered.find(".resume-projects-entry__basics > .hide-on-med-and-up > .resume-projects-entry__date").html()).to.match(/ to Present/);
+    });
+
+    it("renders (`.hide-on-print` if 4th or subsequent project)", function () {
+        const rendered = shallow(<ResumeProjectsEntry projectsEntry={stubResumeProjectsEntry} index={4}/>);
+
+        expect(rendered).to.be.ok;
+        expect(rendered).to.have.className("hide-on-print");
+    });
+
+    it("renders (no `projectsEntry.url`)", function () {
+        delete stubResumeProjectsEntry.url;
+        const rendered = shallow(<ResumeProjectsEntry projectsEntry={stubResumeProjectsEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+
+        expect(rendered.find(CampaignLink)).to.be.ok;
+        expect(rendered.find(CampaignLink)).to.have.length(0);
+        expect(rendered.find(".resume-projects-entry__name")).to.contain(
+            <span className="text">{stubResumeProjectsEntry.name}</span>
+        );
+        expect(rendered).to.not.have.descendants(".resume-projects-entry__details > .right.hide-on-small-only");
+    });
+
+    it("renders (no `projectsEntry.highlights`)", function () {
+        delete stubResumeProjectsEntry.highlights;
+        const rendered = shallow(<ResumeProjectsEntry projectsEntry={stubResumeProjectsEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+
+        expect(rendered).to.not.have.descendants(".resume-projects-entry__highlights");
+        expect(rendered).to.not.have.descendants(".resume-projects-entry__highlight");
+    });
+
+    it("renders (no `projectsEntry.roles`)", function () {
+        delete stubResumeProjectsEntry.roles;
+        const rendered = shallow(<ResumeProjectsEntry projectsEntry={stubResumeProjectsEntry} index={0}/>);
+
+        expect(rendered).to.be.ok;
+
+        expect(rendered).to.have.descendants(".resume-projects-entry__details");
+        expect(rendered).to.not.have.descendants(".resume-projects-entry__details > div > .resume-projects-entry__position");
+    });
+});

commit b1def0bd714dc5bfe6a3fbca28a4c081d50e9acc

fix(resume): Fix legal page overflow.
A regression introduced in d7ae34af33dfd9bcfe75888ee5035590ec84e6c7.

packages/resume/public/components/resume/content/education/entry.jsx
@@ -60,7 +60,7 @@ export const ResumeEducationEntry = ({educationEntry, index}) => {
                                 {
                                     educationEntry.courses.map((highlight, index) => {
                                         return <li
-                                            className={`resume-education-entry__highlight${index < 3 ? " show-on-letter show-on-a4" : ""}${index >= 3 ? " show-on-legal" : ""}`}
+                                            className={`resume-education-entry__highlight${index > 3 ? " hide-on-print" : ""}`}
                                             key={index}>
                                             {highlight}
                                         </li>;

packages/resume/test/unit/public/components/resume/content/education/entry.jsx
@@ -48,8 +48,7 @@ describe("ResumeEducationEntry", function () {
         expect(rendered).to.have.descendants(".resume-education-entry__highlights");
         expect(rendered).to.have.descendants(".resume-education-entry__highlight");
         expect(rendered.find(".resume-education-entry__highlight")).to.have.length(stubResumeEducationEntry.courses.length);
-        expect(rendered.find(".resume-education-entry__highlight.show-on-letter.show-on-a4")).to.have.length(3);
-        expect(rendered.find(".resume-education-entry__highlight.show-on-legal")).to.have.length(stubResumeEducationEntry.courses.length - 3);
+        expect(rendered.find(".resume-education-entry__highlight.hide-on-print")).to.have.length(stubResumeEducationEntry.courses.length - 4);
     });
 
     it("renders (no end date)", function () {