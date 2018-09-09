7d625d6e9634ec55a7a43ee96e3eceaca45d960a
Randy Tarampi <randy.tarampi@gmail.com>
2018-09-09 09:40:27 -0700

    chore(resume): Add Coveralls and Code Climate badges.
    And add a second highlight to push the content down a bit.
    
    Close #91.

--- packages/resume/public/components/resume/content/projects/index.jsx
+++ packages/resume/public/components/resume/content/projects/index.jsx
@@ -1,4 +1,4 @@
-import {Printable} from "@randy.tarampi/jsx";
+import {Link, Printable} from "@randy.tarampi/jsx";
 import PropTypes from "prop-types";
 import React, {Fragment} from "react";
 import ResumeProjectsEntry from "./entry";
@@ -17,6 +17,16 @@ export const ResumeProjects = ({resume}) => {
                 </p>
                 <p><span className="text">Frankly, I'd much rather be outside exploring rather than on my computer in my downtime</span>
                 </p>
+                <p><span className="text">Tried to keep myself disciplined here, but I'm a lot better when I have people actually holding me to account</span>
+                </p>
+                <div>
+                    <Link href="https://coveralls.io/github/randytarampi/me" aria-label="Coveralls"
+                          name="Coveralls score"><img
+                        src="https://img.shields.io/coveralls/github/randytarampi/me.svg?style=flat-square"/></Link>
+                    <Link href="https://codeclimate.com/github/randytarampi/me/maintainability"
+                          aria-label="Code Climate score" name="Code Climate"><img
+                        src="https://img.shields.io/codeclimate/maintainability-percentage/randytarampi/me.svg?style=flat-square"/></Link>
+                </div>
             </Fragment>
         }
     >
