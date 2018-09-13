e77d9393861751baa82e1f311c4cee57d5ac1f24
Randy Tarampi <rt@randytarampi.ca>
2018-09-12 21:44:31 -0700

    chore(jsx): Remove unused global selectors.

--- packages/jsx/lib/data/selectors.js
+++ packages/jsx/lib/data/selectors.js
@@ -1,12 +1,11 @@
-import {getApiState, getApiStateForUrl} from "./api";
+import {getApiStateForUrl} from "./api";
 import {getEmoji, hasEmoji} from "./emoji";
-import {getError, getErrorCode, getErrorMessage, getErrorState, hasError} from "./error";
+import {getError, getErrorCode, getErrorMessage, hasError} from "./error";
 import {
     getNewestPost,
     getOldestPost,
     getPhotoPosts,
     getPhotoPostsSortedByDate,
-    getPosts,
     getPostsSortedByDate,
     getWordPosts,
     getWordPostsSortedByDate
@@ -18,9 +17,7 @@ export const selectors = {
     getError: state => getError(state.get("error")),
     getErrorCode: state => getErrorCode(state.get("error")),
     getErrorMessage: state => getErrorMessage(state.get("error")),
-    getErrorState: state => getErrorState(state.get("error")),
 
-    getPosts: state => getPosts(state.get("posts")),
     getPhotoPosts: state => getPhotoPosts(state.get("posts")),
     getWordPosts: state => getWordPosts(state.get("posts")),
     getPostsSortedByDate: state => getPostsSortedByDate(state.get("posts")),
@@ -29,7 +26,6 @@ export const selectors = {
     getOldestPost: state => getOldestPost(state.get("posts")),
     getNewestPost: state => getNewestPost(state.get("posts")),
 
-    getApiState: state => getApiState(state.get("api")),
     getApiStateForUrl: (state, url) => getApiStateForUrl(state.get("api"), url),
 
     getLocation: state => getLocation(state.get("routing")),
