45df3d29a9f4f170c6a765c92cf0f5e8e3927df2
Randy Tarampi <rt@randytarampi.ca>
2018-09-13 00:03:17 -0700

    chore(bin): This is a `NOTE`, not a `FIXME`.

--- bin/postinstall-css.sh
+++ bin/postinstall-css.sh
@@ -6,6 +6,6 @@ REPO_ROOT=${TRAVIS_BUILD_DIR:=$(pwd)}
 
 cd "${REPO_ROOT}";
 
-# FIXME-RT: Consumers of `css` needs it have local references to style assets so we can pull them in during the build
+# NOTE-RT: Consumers of `css` need to have local references to style assets so we can pull them in during the build. It's easier to code in references to local `node_modules` than to guess if they exist above – in preparation for #67 and publishing them
 ln -snf "${REPO_ROOT}/node_modules/materialize-css/" "${REPO_ROOT}/packages/css/node_modules/materialize-css";
 ln -snf "${REPO_ROOT}/node_modules/@fortawesome/" "${REPO_ROOT}/packages/css/node_modules/@fortawesome";
