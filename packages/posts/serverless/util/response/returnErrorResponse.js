0a457b7197b03fb4e03b963ee25ad22a3ff6c113
Randy Tarampi <rt@randytarampi.ca>
2018-09-15 21:47:32 -0700

    chore(posts): Log a `warn` whenever we return a 4xx error.

--- packages/posts/serverless/util/response/returnErrorResponse.js
+++ packages/posts/serverless/util/response/returnErrorResponse.js
@@ -14,7 +14,7 @@ export default callback =>
      */
         error => {
         if (error instanceof RequestError) {
-            logger.info(error, `Returning ${error.statusCode} error response`);
+            logger.warn(error, `Returning ${error.statusCode} error response`);
             callback(null, responseBuilder({
                 error: {
                     message: error.message,
