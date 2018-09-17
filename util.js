9efbd3103c3395e1411dc7c67096d623483ada4f
Randy Tarampi <rt@randytarampi.ca>
2018-09-16 23:56:36 -0700

    fix: Try fixing iOS 6 Safari and IE<=10 brokenness by `babel`ing `libphonenumber-js`.
    I'm surprised that it works in IE11 – I didn't think it supported the ES6 modules we seem to be importing...

--- util.js
+++ util.js
@@ -1,5 +1,5 @@
 module.exports = {
-    babelLoaderExclusions: /\/node_modules\/(?!(?:@randy\.tarampi|query-string|strict-uri-encode|strip-ansi|ansi-regex|react-dimensions|bunyan-sentry-stream)\/)/,
-    babelRegisterInclusions: /\/(?:node_modules\/(?:@randy\.tarampi|query-string|strict-uri-encode|strip-ansi|ansi-regex|react-dimensions|bunyan-sentry-stream)|packages)\//,
+    babelLoaderExclusions: /\/node_modules\/(?!(?:@randy\.tarampi|query-string|strict-uri-encode|strip-ansi|ansi-regex|react-dimensions|bunyan-sentry-stream|libphonenumber-js)\/)/,
+    babelRegisterInclusions: /\/(?:node_modules\/(?:@randy\.tarampi|query-string|strict-uri-encode|strip-ansi|ansi-regex|react-dimensions|bunyan-sentry-stream|libphonenumber-js)|packages)\//,
     webpackVendorInclusions: /\/node_modules\//,
 };
