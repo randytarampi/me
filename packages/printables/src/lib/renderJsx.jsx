b75d12b907778be78fa7aaf133c2f10342234da9
Randy Tarampi <rt@randytarampi.ca>
2018-09-22 22:52:17 -0700

    chore(printables): Don't assume this `pageSize` will be a string.
    It might not be 'cause `babel` seems to be doing something weird here...

--- packages/printables/src/lib/renderJsx.jsx
+++ packages/printables/src/lib/renderJsx.jsx
@@ -3,7 +3,7 @@ import {renderToStaticMarkup} from "react-dom/server";
 import Helmet from "react-helmet";
 
 export const renderJsx = PrintableComponent => ({pageSize, ...props}) => renderToStaticMarkup(
-    <PrintableComponent {...props} pageSize={pageSize && pageSize.toLowerCase()}/>
+    <PrintableComponent {...props} pageSize={pageSize && pageSize.toLowerCase && pageSize.toLowerCase()}/>
 );
 
 export default renderJsx;
