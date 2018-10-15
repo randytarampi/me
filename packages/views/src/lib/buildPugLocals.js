ed317ed8a01555943094a38a47e2ace0e59f21ee
Randy Tarampi <rt@randytarampi.ca>
2018-10-14 22:23:54 -0700

    fix(views): Burn the `.loading-spinner` into the HTML.
    Might as well show something as we load...

--- packages/views/src/lib/buildPugLocals.js
+++ packages/views/src/lib/buildPugLocals.js
@@ -33,6 +33,57 @@ export const buildPugLocals = ({packageJson, helmetContent, ...passedLocals}) =>
         sentryDsn: config.has("sentryDsn") ? config.get("sentryDsn") : null,
         gtm: config.has("gtm") ? config.get("gtm") : null,
         logger: config.has("logger") ? JSON.stringify(config.get("logger")) : null,
+        content: `<div className="loading-spinner">
+    <div className="preloader-wrapper big active">
+        <div className="spinner-layer spinner-blue">
+            <div className="circle-clipper left">
+                <div className="circle"/>
+            </div>
+            <div className="gap-patch">
+                <div className="circle"/>
+            </div>
+            <div className="circle-clipper right">
+                <div className="circle"/>
+            </div>
+        </div>
+
+        <div className="spinner-layer spinner-red">
+            <div className="circle-clipper left">
+                <div className="circle"/>
+            </div>
+            <div className="gap-patch">
+                <div className="circle"/>
+            </div>
+            <div className="circle-clipper right">
+                <div className="circle"/>
+            </div>
+        </div>
+
+        <div className="spinner-layer spinner-yellow">
+            <div className="circle-clipper left">
+                <div className="circle"/>
+            </div>
+            <div className="gap-patch">
+                <div className="circle"/>
+            </div>
+            <div className="circle-clipper right">
+                <div className="circle"/>
+            </div>
+        </div>
+
+        <div className="spinner-layer spinner-green">
+            <div className="circle-clipper left">
+                <div className="circle"/>
+            </div>
+            <div className="gap-patch">
+                <div className="circle"/>
+            </div>
+            <div className="circle-clipper right">
+                <div className="circle"/>
+            </div>
+        </div>
+    </div>
+</div>`,
         ...meLocals,
         ...passedLocals
     };
