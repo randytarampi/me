12285ade1bac6404f93f7909bd2e9ce96eb2baef
Randy Tarampi <rt@randytarampi.ca>
2018-10-25 21:34:19 -0700

    fix: Actually add the files mentioned in 24eff7edf9b2f4f7cad912e03d1b1efacf4022cb.

---
+++ packages/job-applications/src/job-applications/jobApplication.js
@@ -0,0 +1,8 @@
+const resume = require("@randy.tarampi/resume/src/resumes/resume.json");
+const letter = require("@randy.tarampi/letter/src/letters/letter.json");
+
+module.exports = {
+    id: "job-application",
+    letter,
+    resume
+};
