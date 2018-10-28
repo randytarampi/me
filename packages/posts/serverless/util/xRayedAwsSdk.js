77f42c5fbb21d19719f3073303c8d04796287308
Randy Tarampi <randy.tarampi@gmail.com>
2018-10-28 15:26:25 -0700

    chore(posts): Also `AwsXRay.captureHTTPsGlobal`.

--- packages/posts/serverless/util/xRayedAwsSdk.js
+++ packages/posts/serverless/util/xRayedAwsSdk.js
@@ -1,9 +1,11 @@
+import http from "http";
 import AwsXRay from "aws-xray-sdk";
 import Aws from "aws-sdk";
 import logger from "../../lib/logger";
 
 Aws.Config.logger = logger;
 AwsXRay.setLogger(logger);
+AwsXRay.captureHTTPsGlobal(http);
 
 export const XRayedAwsSdk = AwsXRay.captureAWS(Aws);
 
