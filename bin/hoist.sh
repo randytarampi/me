7479c1e7fab886167550b8a3e0952ea167f8d14b
Randy Tarampi <rt@randytarampi.ca>
2018-10-29 17:03:27 -0700

    chore(travis): There is only `lerna bootstrap --hoist`.
    It really is the fastest way to install. We can close #175 now.

---
+++ bin/hoist.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e;
+
+REPO_ROOT=${TRAVIS_BUILD_DIR:=$(pwd)}
+LERNA_CONCURRENCY=${CPUS_COUNT:=$(node -p "require(\"os\").cpus().length")}
+
+cd "${REPO_ROOT}";
+
+# NOTE-RT: Bootstrap the monorepo
+npx lerna bootstrap --hoist --no-ci --concurrency ${LERNA_CONCURRENCY};
+
+"${REPO_ROOT}/bin/hoist-jsonresume-theme.sh";
