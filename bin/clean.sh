4bc154faf3dbc3d199d6aa8cedad6a4e4a23270e
Randy Tarampi <rt@randytarampi.ca>
2018-10-29 17:04:25 -0700

    fix(travis): Fix Lerna's concurrency to 2 on the Travis boxes.
    [Amdhal's law](https://en.wikipedia.org/wiki/Amdahl%27s_law)~~.

--- bin/clean.sh
+++ bin/clean.sh
@@ -3,11 +3,12 @@
 set -e;
 
 REPO_ROOT=${TRAVIS_BUILD_DIR:=$(pwd)}
+LERNA_CONCURRENCY=${CPUS_COUNT:=$(node -p "require(\"os\").cpus().length")}
 
 cd "${REPO_ROOT}";
 
-npx lerna run preuninstall;
+npx lerna run preuninstall --concurrency ${LERNA_CONCURRENCY};
 
-npx lerna clean --yes;
+npx lerna clean --yes --concurrency ${LERNA_CONCURRENCY};
 
 rm -rf node_modules coverage .nyc_output;
