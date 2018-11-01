b57372918b7b8ce8268aab88962c4701d5a17b23
Randy Tarampi <rt@randytarampi.ca>
2018-11-01 01:06:28 -0700

    fix(jsonresume-theme): Add a bunch more `pugLocals` driven off the `resume.json`.

--- packages/jsonresume-theme/src/index.js
+++ packages/jsonresume-theme/src/index.js
@@ -3,14 +3,38 @@ import config from "config";
 import path from "path";
 
 export const render = (resumeJson, pageSize) => {
+    const resume = Resume.fromResume(resumeJson);
     return renderHtml({
-        printable: Resume.fromResume(resumeJson),
+        logger: null,
+        printable: resume,
         pageSize,
         assetUrl: config && config.has("resume.assetUrl")
             ? config.get("resume.assetUrl")
             : "",
         printableStylesPath: path.join(__dirname, "../node_modules/@randy.tarampi/resume/dist/styles.css"),
-        printableTemplatePath: path.join(__dirname, "../node_modules/@randy.tarampi/views/templates/index.pug")
+        printableTemplatePath: path.join(__dirname, "../node_modules/@randy.tarampi/views/templates/index.pug"),
+        pugTitle: resume.basics.name,
+        pugDescription: resume.basics.summary,
+        meImageUrl: resume.basics.picture,
+        pageUrl: resume.basics.website,
+        twitterUsername: config && config.has("me.profiles.Twitter.username")
+            ? config.get("me.profiles.Twitter.username")
+            : resumeJson.twitter && resumeJson.twitter.user && resumeJson.twitter.user.username,
+        facebookUsername: config && config.has("me.profiles.Facebook.username")
+            ? config.get("me.profiles.Facebook.username")
+            : resumeJson.facebook && resumeJson.facebook.user && resumeJson.facebook.user.username,
+        facebookAppId: config && config.has("facebook.app.id")
+            ? config.get("facebook.app.id")
+            : resumeJson.facebook && resumeJson.facebook.app && resumeJson.facebook.app.id,
+        gtmContainerId: config && config.has("gtm.container.id")
+            ? config.get("gtm.container.id")
+            : resumeJson.gtm && resumeJson.gtm.container && resumeJson.gtm.container.id,
+        gaPropertyId: config && config.has("ga.property.id")
+            ? config.get("ga.property.id")
+            : resumeJson.ga && resumeJson.ga.property && resumeJson.ga.property.id,
+        mixpanelAppId: config && config.has("mixpanel.app.id")
+            ? config.get("mixpanel.app.id")
+            : resumeJson.mixpanel && resumeJson.mixpanel.app && resumeJson.mixpanel.app.id,
     });
 };
 
