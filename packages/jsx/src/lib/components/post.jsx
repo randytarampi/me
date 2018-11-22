0816a15aff5ec9ca27af6e6012b1703160c373bb
Randy Tarampi <rt@randytarampi.ca>
2018-11-22 11:33:34 -0800

    feat(jsx): Show tags for `Post`s, per #76.

--- packages/jsx/src/lib/components/post.jsx
+++ packages/jsx/src/lib/components/post.jsx
@@ -3,7 +3,7 @@ import SchemaJsonLdComponent from "@randy.tarampi/schema-dot-org-json-ld-compone
 import isHtml from "is-html";
 import {DateTime} from "luxon";
 import PropTypes from "prop-types";
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import {Col, Row} from "react-materialize";
 import {CampaignLink} from "./link";
 
@@ -39,11 +39,13 @@ export class PostComponent extends Component {
     }
 
     render() {
+        const {post} = this.props;
+        
         return <Row
             className="post post--words"
-            id={this.props.post.uid}
+            id={post.uid}
         >
-            <SchemaJsonLdComponent markup={this.props.post.toSchema()}/>
+            <SchemaJsonLdComponent markup={post.toSchema()}/>
             <Col
                 className="post-metadata"
                 s={12}
@@ -51,9 +53,9 @@ export class PostComponent extends Component {
             >
                 <h1 className="post-title">
                     {
-                        this.props.post.sourceUrl ?
+                        post.sourceUrl ?
                             <CampaignLink className="post-title__link"
-                                          href={this.props.post.sourceUrl}>{this.title}</CampaignLink> :
+                                          href={post.sourceUrl}>{this.title}</CampaignLink> :
                             <span className="post-title__text">{this.title}</span>
                     }
                 </h1>
@@ -63,13 +65,25 @@ export class PostComponent extends Component {
                         className="post-date__date post-date__date--published">{this.date.toLocaleString(DateTime.DATE_MED)}</span>
                 </p>
                 {
-                    this.props.post.dateCreated ?
+                    post.dateCreated ?
                         <p className="post-date">
                             <strong className="post-date__label post-date__label--created">Created:</strong>
                             <span
-                                className="post-date__date post-date__date--created">{this.props.post.dateCreated.toLocaleString(DateTime.DATETIME_MED)}</span>
+                                className="post-date__date post-date__date--created">{post.dateCreated.toLocaleString(DateTime.DATETIME_MED)}</span>
                         </p> :
                         null
+                }
+                {
+                    post.tags && post.tags.size
+                        ? <p className="post-tags">
+                            <strong className="post-tags__label">Tags:</strong>
+                            {
+                                post.tags.map(tag => <Fragment key={tag}>
+                                    <span className="post-tags__tag">{tag}</span>&ensp;
+                                </Fragment>)
+                            }
+                        </p>
+                        : null
                 }
             </Col>
             <Col
@@ -78,21 +92,21 @@ export class PostComponent extends Component {
                 l={8}
             >
                 {
-                    typeof this.props.post.body === "string"
+                    typeof post.body === "string"
                         ? <div className="post-body">
                             {
-                                isHtml(this.props.post.body)
+                                isHtml(post.body)
                                     ? <div className="post-body__html">
-                                        <div dangerouslySetInnerHTML={{__html: this.props.post.body}}/>
+                                        <div dangerouslySetInnerHTML={{__html: post.body}}/>
                                     </div>
-                                    : <p><span className="post-body__text">{this.props.post.body}</span></p>
+                                    : <p><span className="post-body__text">{post.body}</span></p>
                             }
                         </div> :
                         null
                 }
                 {
-                    Array.isArray(this.props.post.body)
-                        ? this.props.post.body.map((maybeHtmlString, index) => {
+                    Array.isArray(post.body)
+                        ? post.body.map((maybeHtmlString, index) => {
                             return <div className="post-body" key={index}>
                                 {
                                     isHtml(maybeHtmlString)
