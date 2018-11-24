0d5dd2d4f0161a20c76a7c933189ca5bd33b6704
Randy Tarampi <rt@randytarampi.ca>
2018-11-24 15:42:47 -0800

    perf(jsx): Don't reduce the entire `route` into state on `SET_ROUTES`.

--- packages/jsx/src/lib/actions/routing/setRoutes.js
+++ packages/jsx/src/lib/actions/routing/setRoutes.js
@@ -2,8 +2,23 @@ import {createAction} from "redux-actions";
 
 export const SET_ROUTES = "SET_ROUTES";
 
+const sanitizeRouteForState = ({routes, tab, component, ...route}) => { // eslint-disable-line no-unused-vars
+    const routeProps = {
+        ...route,
+        tab: !!tab
+    };
+
+    if (routes) {
+        routeProps.routes = sanitizeRoutesForState(routes);
+    }
+
+    return routeProps;
+};
+
+const sanitizeRoutesForState = routes => routes.map(sanitizeRouteForState);
+
 export const setRoutesCreator = routes => dispatch => {
-    dispatch(setRoutes(routes));
+    dispatch(setRoutes(sanitizeRoutesForState(routes)));
 };
 
 export const setRoutes = createAction(SET_ROUTES);
--- packages/jsx/test/unit/src/lib/actions/routing/setRoutes.js
+++ packages/jsx/test/unit/src/lib/actions/routing/setRoutes.js
@@ -19,14 +19,60 @@ describe("setRoutes", function () {
 
     describe("SET_ROUTES", function () {
         it("is dispatched with the expected payload", function () {
-            const stubPayload = "woof";
+            const stubPayload = [
+                {
+                    tab: "foo",
+                    path: "meow",
+                    component: true
+                },
+                {
+                    component: true,
+                    tab: "bar",
+                    path: "grr",
+                    exact: true,
+                    routes: [
+                        {
+                            tab: 1,
+                            component: 1,
+                            path: "rawr",
+                            parent: {
+                                path: "grr",
+                                tab: true
+                            }
+                        }
+                    ]
+                }
+            ];
             stubStore.dispatch(setRoutes(stubPayload));
 
             const actions = stubStore.getActions();
 
             expect(actions).to.be.ok;
             expect(actions).to.have.length(1);
-            expect(actions).to.eql([{type: SET_ROUTES, payload: stubPayload}]);
+            expect(actions).to.eql([{
+                type: SET_ROUTES,
+                payload: [
+                    {
+                        tab: true,
+                        path: "meow"
+                    },
+                    {
+                        tab: true,
+                        path: "grr",
+                        exact: true,
+                        routes: [
+                            {
+                                path: "rawr",
+                                tab: true,
+                                parent: {
+                                    path: "grr",
+                                    tab: true
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }]);
         });
     });
 });
