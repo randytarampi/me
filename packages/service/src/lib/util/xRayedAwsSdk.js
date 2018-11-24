a77660b1f302a38370affa342fac918e621b0e92
Randy Tarampi <rt@randytarampi.ca>
2018-11-24 14:57:27 -0800

    fix(service): Don't run X-Ray when `IS_OFFLINE` or in `test`.

--- packages/service/src/lib/util/xRayedAwsSdk.js
+++ packages/service/src/lib/util/xRayedAwsSdk.js
@@ -1,12 +1,18 @@
 import Aws from "aws-sdk";
-import AwsXRay from "aws-xray-sdk";
 import http from "http";
 import logger from "../logger";
 
 Aws.Config.logger = logger;
-AwsXRay.setLogger(logger);
-AwsXRay.captureHTTPsGlobal(http);
 
-export const XRayedAwsSdk = AwsXRay.captureAWS(Aws);
+let AwsClient = Aws;
+
+if (!process.env.IS_OFFLINE && process.env.NODE_ENV !== "test") {
+    const AwsXRay = require("aws-xray-sdk");
+    AwsXRay.setLogger(logger);
+    AwsXRay.captureHTTPsGlobal(http);
+    AwsClient = AwsXRay.captureAWS(Aws);
+}
+
+export const XRayedAwsSdk = AwsClient;
 
 export default XRayedAwsSdk;
