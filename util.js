d2f1eb5b678cefa7d47df328cbf8f2b562e11992
Randy Tarampi <rt@randytarampi.ca>
2018-11-28 13:18:50 -0800

    chore: Take `BABEL_ENV` into account when computing `isDevelopment`.

--- util.js
+++ util.js
@@ -1,6 +1,9 @@
-const productionNodeEnvs = ["production", "prd"];
-const nodeEnvIsNotProduction = !productionNodeEnvs.includes(process.env.NODE_ENV);
-const isDevelopment = nodeEnvIsNotProduction;
+const productionEnvs = ["production", "prd"];
+const isNodeEnvDevelopment = !productionEnvs.includes(process.env.NODE_ENV);
+const isBabelEnvDevelopment = process.env.BABEL_ENV !== undefined
+    ? !productionEnvs.includes(process.env.BABEL_ENV)
+    : isNodeEnvDevelopment;
+const isDevelopment = isNodeEnvDevelopment && isBabelEnvDevelopment;
 
 const WEBPACK_MODE_PRODUCTION = "production";
 const WEBPACK_MODE_DEVELOPMENT = "development";
@@ -17,9 +20,10 @@ const webpackMode = resolveWebpackMode();
 module.exports = {
     WEBPACK_MODE_PRODUCTION,
     WEBPACK_MODE_DEVELOPMENT,
-    productionNodeEnvs,
-    nodeEnvIsNotProduction,
+    productionEnvs,
     isDevelopment,
+    isNodeEnvDevelopment,
+    isBabelEnvDevelopment,
     resolveWebpackMode,
     webpackMode,
     webpackNodeExternalsWhitelist: /@randy\.tarampi|query-string|strict-uri-encode|strip-ansi|ansi-regex|bunyan-sentry-stream|libphonenumber-js|react-router-sitemap|react-hot-loader|dom-helpers|redux-immutable|reduce-reducers|react-router|react-progressive-image|react-metrics|react-materialize|react-event-listener|react-helmet/,
