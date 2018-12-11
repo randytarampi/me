dbdc4f81b11a9ce6a937a29032b4cf71ce3f3d92
Randy Tarampi <rt@randytarampi.ca>
2018-12-11 10:06:56 -0800

    chore(jsx): Remove unnecessary `sniffr.browser` check.

--- packages/jsx/src/lib/clientRoot.jsx
+++ packages/jsx/src/lib/clientRoot.jsx
@@ -1,8 +1,8 @@
 import {logger} from "@randy.tarampi/browser-logger";
+import {config as metricsConfig} from "@randy.tarampi/redux-metrics";
 import React, {Component} from "react";
 import {metrics} from "react-metrics";
 import Sniffr from "sniffr";
-import {config as metricsConfig} from "@randy.tarampi/redux-metrics";
 
 export class ClientRoot extends Component {
     constructor(props) {
@@ -16,16 +16,14 @@ export class ClientRoot extends Component {
         const sniffr = new Sniffr();
         sniffr.sniff();
 
-        if (sniffr.browser) {
-            if (sniffr.browser.name === "firefox") {
-                logger.info("If you don't already have them, these should make your analysis a bit more interesting.\n\t\thttps://addons.mozilla.org/en-US/firefox/addon/react-devtools\n\t\thttps://addons.mozilla.org/en-US/firefox/addon/remotedev");
-            } else if (sniffr.browser.name === "chrome") {
-                logger.info("If you don't already have them, these should make your analysis a bit more interesting.\n\t\thttps://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi\n\t\thttps://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd");
-            } else if (sniffr.browser.name === "ie") {
-                logger.warn("Do yourself a favour and go here before you do anything else:\n\t\thttp://outdatedbrowser.com");
-            } else {
-                logger.warn("If you're a developer and you're reading this message, do the right thing, give me a fair shake and come back in Chrome or Firefox.\n\t\thttps://www.mozilla.org/firefox\n\t\thttps://www.google.com/chrome");
-            }
+        if (sniffr.browser.name === "firefox") {
+            logger.info("If you don't already have them, these should make your analysis a bit more interesting.\n\t\thttps://addons.mozilla.org/en-US/firefox/addon/react-devtools\n\t\thttps://addons.mozilla.org/en-US/firefox/addon/remotedev");
+        } else if (sniffr.browser.name === "chrome") {
+            logger.info("If you don't already have them, these should make your analysis a bit more interesting.\n\t\thttps://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi\n\t\thttps://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd");
+        } else if (sniffr.browser.name === "ie") {
+            logger.warn("Do yourself a favour and go here before you do anything else:\n\t\thttp://outdatedbrowser.com");
+        } else {
+            logger.warn("If you're a developer and you're reading this message, do the right thing, give me a fair shake and come back in Chrome or Firefox.\n\t\thttps://www.mozilla.org/firefox\n\t\thttps://www.google.com/chrome");
         }
     }
 
