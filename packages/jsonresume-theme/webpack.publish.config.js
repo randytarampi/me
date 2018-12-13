8de800890356c23c77e49aa5cd7e448da29121bf
Randy Tarampi <rt@randytarampi.ca>
2018-12-13 10:14:59 -0800

    fix(jsonresume-theme): Don't bother with `webpack-node-externals`.
    Just pack everything in and let webpack stub out the actual node internal libs.

--- packages/jsonresume-theme/webpack.publish.config.js
+++ packages/jsonresume-theme/webpack.publish.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
 const webpackBaseConfig = require("../../webpack.publish.config.base");
-const {webpackNodeExternalsWhitelist} = require("../../util");
-const nodeExternals = require("webpack-node-externals");
 
 module.exports = webpackBaseConfig({
     babelEnv: "client.es5",
@@ -11,11 +9,5 @@ module.exports = webpackBaseConfig({
         index: ["@babel/polyfill", "raf/polyfill", path.join(__dirname, "./src/index.js")]
     },
     node: false,
-    target: "node",
-    externals: [nodeExternals({
-        whitelist: [
-            /@randy\.tarampi\//,
-            webpackNodeExternalsWhitelist
-        ]
-    })]
+    target: "node"
 });
