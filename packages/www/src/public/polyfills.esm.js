799c175ab1911eb2d50a2fd95db8b674746e357b
Randy Tarampi <rt@randytarampi.ca>
2018-12-13 23:44:00 -0800

    feat(www): Serve webpacked ESM code for clients that accept it.
    Pretty much everyone these days. A whole 10kb saved though.

---
+++ packages/www/src/public/polyfills.esm.js
@@ -0,0 +1,2 @@
+global.tweenedOpacity = undefined; // NOTE-RT: Need this for the ESM build 'cause there's no `tweenedOpacity` defined in https://github.com/Dogfalo/materialize/blob/v0.100.2/js/carousel.js
+global.zTranslation = undefined; // NOTE-RT: Need this for the ESM build 'cause there's no `zTranslation` defined in https://github.com/Dogfalo/materialize/blob/v0.100.2/js/carousel.js
--- packages/www/webpack.client.config.esm.js
+++ packages/www/webpack.client.config.esm.js
@@ -4,6 +4,7 @@ const path = require("path");
 process.env.NODE_CONFIG_DIR = path.join(__dirname, "../../config");
 
 const config = require("config");
+const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer");
 const webpackBaseConfig = require("./webpack.client.config.base");
 const WorkboxPlugin = require("workbox-webpack-plugin");
 const packageJson = require("./package");
@@ -12,22 +13,37 @@ const TerserPlugin = require("terser-webpack-plugin");
 const {DefinePlugin} = require("webpack");
 
 const publicPath = `${config.get("www.assetUrl")}/`;
-const bundleName = config.get("www.bundle.name");
-const swBundleName = config.get("www.bundle.sw");
-const swBundleInstallerName = config.get("www.bundle.swInstaller");
+const bundleName = `${config.get("www.bundle.name")}.esm`;
+const swBundleName = `${config.get("www.bundle.sw")}.esm`;
+const swBundleInstallerName = `${config.get("www.bundle.swInstaller")}.esm`;
 
 const {
     isDevelopment
 } = util;
 
 module.exports = webpackBaseConfig({
-    babelLoaderExclusions: /\/node_modules\/(?:react|react-dom)\//,
     babelEnv: "client.esm",
+    // babelJsType: "javascript/esm",
+    rules: [
+        {
+            test: require.resolve("jquery"),
+            use: [
+                {
+                    loader: "expose-loader",
+                    options: "jQuery"
+                },
+                {
+                    loader: "expose-loader",
+                    options: "$"
+                }
+            ]
+        }
+    ],
 
     entry: {
-        [`${bundleName}.esm`]: ["@babel/polyfill", "raf/polyfill", "jquery", "materialize-css", path.join(__dirname, "src/public/views/index.jsx")],
-        [`${swBundleInstallerName}.esm`]: path.join(__dirname, "src/public/sw/installer.js")
-        // styles: path.join(__dirname, "./styles/style.scss")
+        [bundleName]: ["@babel/polyfill", "raf/polyfill", "jquery", "materialize-css", path.join(__dirname, "src/public/polyfills.esm.js"), path.join(__dirname, "src/public/views/index.jsx")],
+        [swBundleInstallerName]: ["@babel/polyfill", "raf/polyfill", "jquery", "materialize-css", path.join(__dirname, "src/public/sw/installer.js")],
+        styles: path.join(__dirname, "./styles/style.scss")
     },
     optimization: {
         splitChunks: {
@@ -53,8 +69,13 @@ module.exports = webpackBaseConfig({
                 ]
     },
     plugins: [
+        new BundleAnalyzerPlugin({
+            reportFilename: "report.esm.html",
+            analyzerMode: "static",
+            openAnalyzer: false
+        }),
         new WorkboxPlugin.GenerateSW({
-            swDest: `${swBundleName}.esm.js`,
+            swDest: `${swBundleName}.js`,
             skipWaiting: true,
             clientsClaim: true,
             offlineGoogleAnalytics: false,
