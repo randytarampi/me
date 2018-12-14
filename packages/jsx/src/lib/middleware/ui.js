19c9b6ad5a13e46d19ce3c59282f89f2d2d63c39
Randy Tarampi <rt@randytarampi.ca>
2018-12-14 12:14:30 -0800

    revert: fix(jsx): Better keep `.nav-tabs__swipeable` in sync with redux state.
    This reverts commit 2e247170b069f5b220ff750f9cfca2b66a02856f.

--- packages/jsx/src/lib/middleware/ui.js
+++ packages/jsx/src/lib/middleware/ui.js
@@ -1,33 +1,26 @@
-/* global $ */
-
 import {LOCATION_CHANGE} from "connected-react-router";
+import $ from "jquery";
 import clearError from "../actions/clearError";
 import {SWIPEABLE_CHANGE_INDEX, SWIPEABLE_TAB_CHANGE_INDEX} from "../actions/routing";
 import selectors from "../data/selectors";
 import {formatIndexForMaterializeTabs} from "../util";
 
-const SWIPEABLE_TABS_CLASS = ".nav-tabs__swipeable";
-
-const setSwipeableTabForState = (swipeableTabs = $(SWIPEABLE_TABS_CLASS), store, action) => {
-    if (swipeableTabs.tabs) {
-        const state = store.getState();
-        const location = action.payload.location || action.payload;
-        const index = selectors.getIndexForRoute(state, location.pathname);
-        const expectedTabIndex = formatIndexForMaterializeTabs(index);
-
-        swipeableTabs.tabs("select_tab", `tab_${expectedTabIndex}`);
-    } else {
-        setTimeout(() => setSwipeableTabForState(swipeableTabs, store, action), 50);
-    }
-};
-
 export const uiMiddleware = store => next => action => {
     switch (action.type) {
         case LOCATION_CHANGE: {
-            const swipeableTabs = $(SWIPEABLE_TABS_CLASS);
+            const swipeableTabs = $(".nav-tabs__swipeable");
 
-            if (swipeableTabs) {
-                setSwipeableTabForState(swipeableTabs, store, action);
+            if (swipeableTabs.tabs) {
+                const state = store.getState();
+                const location = action.payload.location || action.payload;
+                const index = selectors.getIndexForRoute(state, location.pathname);
+                const expectedTabIndex = formatIndexForMaterializeTabs(index);
+
+                if (swipeableTabs.length) {
+                    swipeableTabs.tabs("select_tab", `tab_${expectedTabIndex}`);
+                } else {
+                    setTimeout(() => $(".nav-tabs__swipeable").tabs("select_tab", `tab_${expectedTabIndex}`), 50);
+                }
             }
             break;
         }
