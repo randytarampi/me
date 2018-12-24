c1e615bfa172ff7e98ceb3b6b203c2fe52e57858
Randy Tarampi <rt@randytarampi.ca>
2018-12-23 22:46:32 -0800

    feat(www): Serve ESM code.
    Let's save the 10kb.

--- packages/www/webpack.client.config.base.js
+++ packages/www/webpack.client.config.base.js
@@ -30,7 +30,7 @@ const buildViewForPageUrl = (pageName, pageUrl = config.get("www.publishUrl")) =
         template: "node_modules/@randy.tarampi/views/templates/index.pug",
         templateParameters: buildPugLocals({
             bundleName: config.get("www.bundle.name"),
-            // esmBundleName: `${config.get("www.bundle.name")}.esm`, // NOTE-RT: Not quite ready for primetime yet.
+            esmBundleName: `${config.get("www.bundle.name")}.esm`,
             serviceWorkerInstallerBundleName: config.get("www.bundle.swInstaller"),
             packageJson,
             pageUrl,
@@ -42,7 +42,13 @@ const buildViewForPageUrl = (pageName, pageUrl = config.get("www.publishUrl")) =
         excludeChunks: [
             "styles",
             "vendor",
-            "www"
+            config.get("www.bundle.name"),
+            config.get("www.bundle.sw"),
+            config.get("www.bundle.swInstaller"),
+            "vendor.esm",
+            `${config.get("www.bundle.name")}.esm`,
+            `${config.get("www.bundle.sw")}.esm`,
+            `${config.get("www.bundle.swInstaller")}.esm`
         ]
     });
 };
