436fdeac1790e95cffeb8b63fde121689d6f9ce6
Randy Tarampi <rt@randytarampi.ca>
2019-04-21 10:52:33 -0700

    revert: revert: release(travis): Release v3.11.0.
    One more time.
    
    BREAKING CHANGE: See the notes in [feat(greenkeeper): react-materialize@3.1.1.](https://github.com/randytarampi/me/commit/3f0924d57d46fa7b28447b72a6bdfe95e8ec8e78).
    
    This reverts commit aed497856530622a451b0a8c645a62ae656451cf.

---
+++ packages/www/docs/precache-manifest.49cc411dd9e3b62d55e4f4d19f69a9c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1e2d4b078119c8b78e",
+    "url": "/styles.css"
+  },
+  {
+    "revision": "2f1e2d4b078119c8b78e",
+    "url": "/styles.js"
+  },
+  {
+    "revision": "13730773f570915f2c2b",
+    "url": "/vendor.esm.js"
+  },
+  {
+    "revision": "8a752e84b8bcb1f1fe6b",
+    "url": "/www.esm.js"
+  },
+  {
+    "revision": "285155477c284bb8e937",
+    "url": "/www.sw.installer.esm.js"
+  }
+]);
