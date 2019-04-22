5a51a3509e2753d1508902863e5bbe5b61ef250e
Randy Tarampi <rt@randytarampi.ca>
2019-04-22 12:15:54 -0700

    fix(jsx): Tab selection updates on keyboard press.
    This isn't a great solution, but it works.

--- packages/jsx/src/lib/middleware/ui.js
+++ packages/jsx/src/lib/middleware/ui.js
@@ -6,9 +6,18 @@ import {formatIndexForMaterializeTabs} from "../util";
 
 const getSwipeableTabs = () => {
     const swipeableTabsElement = document.getElementsByClassName("nav-tabs__swipeable")[0];
-    const swipeableTabs = swipeableTabsElement && window.M && window.M.Tabs.getInstance(swipeableTabsElement);
 
-    return swipeableTabs;
+    return swipeableTabsElement && window.M && window.M.Tabs.getInstance(swipeableTabsElement);
+};
+
+const getSwipeableTabsExpectedTabIndex = (state, action) => {
+    const location = action.payload.location || action.payload;
+
+    return selectors.getIndexForRoute(state, location.pathname);
+};
+
+const getSwipeableTabsExpectedTabId = (store, action) => {
+    return `tab_${formatIndexForMaterializeTabs(getSwipeableTabsExpectedTabIndex(store, action))}`;
 };
 
 export const uiMiddleware = store => next => action => {
@@ -17,17 +26,21 @@ export const uiMiddleware = store => next => action => {
             const swipeableTabs = getSwipeableTabs();
 
             if (swipeableTabs) {
-                swipeableTabs.updateTabIndicator();
+                const state = store.getState();
+
+                if (swipeableTabs.index !== getSwipeableTabsExpectedTabIndex(state, action)) {
+                    swipeableTabs.select(getSwipeableTabsExpectedTabId(state, action));
+                }
             } else {
                 setTimeout(() => {
                     const swipeableTabs = getSwipeableTabs();
 
                     if (swipeableTabs) {
                         const state = store.getState();
-                        const location = action.payload.location || action.payload;
-                        const index = selectors.getIndexForRoute(state, location.pathname);
-                        const expectedTabIndex = formatIndexForMaterializeTabs(index);
-                        swipeableTabs.select(`tab_${expectedTabIndex}`);
+
+                        if (swipeableTabs.index !== getSwipeableTabsExpectedTabIndex(state, action)) {
+                            swipeableTabs.select(getSwipeableTabsExpectedTabId(state, action));
+                        }
                     }
                 }, 50);
             }
