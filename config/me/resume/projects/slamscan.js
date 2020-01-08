b24c5e5541d67941edc0eb67579f970510d50610
Randy Tarampi <rt@randytarampi.ca>
2020-01-08 07:00:50 +0100

    chore(travis): Make node@6 testable.
    Even though it'll fail for sure, right?

---
+++ config/me/resume/projects/slamscan.js
@@ -0,0 +1,24 @@
+const fetchAuto = require("../../organizations/fetchAuto");
+
+module.exports = {
+    entity: fetchAuto,
+    name: "S3 Lambda Clamscan",
+    description: "A set of AWS Lambdas that scans files uploaded to an S3 bucket for viruses and other threats",
+    highlights: [
+        "Open up Chrome and try to print it out – follow the instructions at the bottom of the page and you might be surprised",
+    ],
+    keywords: [
+        "ES6",
+        "react",
+        "sass",
+        "Print styles",
+        "Puppeteer"
+    ],
+    startDate: "2018-08-01",
+    roles: [
+        "👨‍💻",
+        "📝"
+    ],
+    type: "Application",
+    url: "https://www.randytarampi.ca/letter"
+};
