2ed01722a28725625e65b96bbdc6df5477e4f97d
Randy Tarampi <rt@randytarampi.ca>
2020-04-23 21:58:45 +0200

    fix(www): Fix a `Tabs` regression from `react-materialize@3.7.0`.
    They changed the signature of `onSelect` in https://github.com/react-materialize/react-materialize/pull/1079/files#diff-f8f6138478fd4cda2a0f875f28829252R38...
    
    :sadpanda:

--- packages/jsx/src/lib/actions/routing/swipeableTabChangeIndex.js
+++ packages/jsx/src/lib/actions/routing/swipeableTabChangeIndex.js
@@ -4,10 +4,11 @@ import selectors from "../../data/selectors";
 
 export const SWIPEABLE_TAB_CHANGE_INDEX = "SWIPEABLE_TAB_CHANGE_INDEX";
 
-export const swipeableTabChangeIndexCreator = tabIndex => (dispatch, getState) => {
+export const swipeableTabChangeIndexCreator = onChangeEvent => (dispatch, getState) => {
+    const tabIndex = onChangeEvent.currentTarget.getAttribute("href").match(/\d+/g)[0];
     const passedTabIndex = Number(tabIndex);
     // NOTE-RT: We need the un`scoped` value here, per https://github.com/react-materialize/react-materialize/commit/f5d1e0d5b97ae4435d6a709b9fc030458fe30b9e#diff-f8f6138478fd4cda2a0f875f28829252R50.
-    const index = passedTabIndex % Math.max((10 ** Math.floor(Math.log10(passedTabIndex))), 10);
+    const index = passedTabIndex % Math.min((10 ** Math.floor(Math.log10(passedTabIndex))), 10);
     dispatch(swipeableTabChangeIndex({index}));
 
     const state = getState();
