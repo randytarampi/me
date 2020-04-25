0116b4874d5caff40b9dc4df316131a0e25e65bc
Randy Tarampi <rt@randytarampi.ca>
2020-04-25 17:12:57 +0200

    chore(package): Use `yarn@2`.
    Maintain the current `node-modules` setup for now though...

--- bin/install.sh
+++ bin/install.sh
@@ -3,9 +3,7 @@
 set -e;
 
 REPO_ROOT=${TRAVIS_BUILD_DIR:=$(pwd)}
-LERNA_CONCURRENCY=${CPUS_COUNT:=$(node -p "require(\"os\").cpus().length")}
 
 cd "${REPO_ROOT}";
 
-npx lerna bootstrap --concurrency "${LERNA_CONCURRENCY}";
 "${REPO_ROOT}/bin/hoist-jsonresume-theme.sh";
