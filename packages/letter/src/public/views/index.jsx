9df6b33ad06e68307949edfc425c09b87ba6005f
Randy Tarampi <rt@randytarampi.ca>
2026-06-23 21:15:20 +0200

    feat(letter): migrate to React 19 + react-router v7

--- packages/letter/src/public/views/index.jsx
+++ packages/letter/src/public/views/index.jsx
@@ -1,10 +1,15 @@
 import "@babel/polyfill";
 
 import React from "react";
-import {render} from "react-dom";
+import {createRoot} from "react-dom/client";
+import {flushSync} from "react-dom";
 import HotApp from "./hotApp";
 
-render(
-    <HotApp/>,
-    document.getElementById("react-root")
-);
+// NOTE-RT: `react-dom@19`'s `createRoot(...).render(...)` schedules the initial mount asynchronously (unlike the
+// NOTE-RT: removed, synchronous `ReactDOM.render`). Flush it synchronously so consumers (and the entry-point tests)
+// NOTE-RT: observe the mounted DOM immediately.
+const root = createRoot(document.getElementById("react-root"));
+
+flushSync(() => {
+    root.render(<HotApp/>);
+});
--- packages/letter/test/unit/src/lib/containers/letter.jsx
+++ packages/letter/test/unit/src/lib/containers/letter.jsx
@@ -4,7 +4,7 @@ import {expect} from "chai";
 import {Map} from "immutable";
 import React from "react";
 import configureStore from "redux-mock-store";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
 import sinon from "sinon";
 import * as fetchLetter from "../../../../../src/lib/actions/fetchLetter";
 import {ConnectedLetter} from "../../../../../src/lib/containers/letter";
