06cdbf34b2425ed20d8d06df69a4d79fcd0be527
Randy Tarampi <rt@randytarampi.ca>
2026-06-29 19:50:24 +0200

    fix(jsx): drop path builtin from reactRouter.cjs. Webpack 5 doesn't polyfill node core modules and Can't resolve 'path' kills the browser build. String ops do the same job.

--- packages/jsx/src/lib/reactRouter.cjs
+++ packages/jsx/src/lib/reactRouter.cjs
@@ -1,15 +1,13 @@
 exports.__esModule = true;
 
-const path = require("path");
-
-const reactRouterRoot = path.dirname(require.resolve("react-router/package.json"));
 const reactRouterBuild = process.env.NODE_ENV === "development" ? "development" : "production";
-const reactRouterDist = path.join(reactRouterRoot, "dist", reactRouterBuild);
+const reactRouterRoot = require.resolve("react-router/package.json").replace(/package\.json$/, "");
+const reactRouterDist = `${reactRouterRoot}dist/${reactRouterBuild}`;
 
-const components = require(path.join(reactRouterDist, "lib/components.js"));
-const hooks = require(path.join(reactRouterDist, "lib/hooks.js"));
-const routerUtils = require(path.join(reactRouterDist, "lib/router/utils.js"));
-const domServer = require(path.join(reactRouterDist, "lib/dom/server.js"));
+const components = require(`${reactRouterDist}/lib/components.js`);
+const hooks = require(`${reactRouterDist}/lib/hooks.js`);
+const routerUtils = require(`${reactRouterDist}/lib/router/utils.js`);
+const domServer = require(`${reactRouterDist}/lib/dom/server.js`);
 
 exports.Router = components.Router;
 exports.Route = components.Route;
