3a625333c48faaf29969e0c150fceb1c1ed9f486
Randy Tarampi <rt@randytarampi.ca>
2026-07-11 09:26:35 +0200

    test: remove deleted social source handler and oAuthClient test files
    Remove test files for instagram, twitter, facebook auth handlers and
    oAuthClient (only used by Twitter). Rewrite sources/index test to
    test only remaining sources (Flickr, S3, Tumblr, Unsplash).

--- packages/service/test/unit/src/lib/sources/index.js
+++ packages/service/test/unit/src/lib/sources/index.js
@@ -2,64 +2,17 @@ import {expect} from "chai";
 import sinon from "sinon";
 import AuthInfo from "../../../../../src/lib/authInfo.js";
 import sources, {initializeSources} from "../../../../../src/lib/sources/index.js";
-import {FacebookSource} from "../../../../../src/lib/sources/facebook/index.js";
-import {InstagramSource} from "../../../../../src/lib/sources/instagram/index.js";
-import {TwitterSource} from "../../../../../src/lib/sources/twitter/index.js";
 
 describe("sources", function () {
     describe("initializeSources", function () {
-        let stubFacebookAuthInfoClientGetter;
-        let stubInstagramAuthInfoClientGetter;
-        let stubTwitterAuthInfoClientGetter;
         let originalFlickrApiKey;
 
         beforeEach(function () {
             originalFlickrApiKey = process.env.FLICKR_API_KEY;
             process.env.FLICKR_API_KEY = "flickr-key";
-
-            class StubFacebookAuthInfoClient {
-                getRecords(searchParams) {
-                    try {
-                        expect(searchParams.source).to.eql(sources.facebook.type);
-
-                        return Promise.resolve([]);
-                    } catch (error) {
-                        return Promise.reject(error);
-                    }
-                }
-            }
-            class StubInstagramAuthInfoClient {
-                getRecords(searchParams) {
-                    try {
-                        expect(searchParams.source).to.eql(sources.instagram.type);
-
-                        return Promise.resolve([]);
-                    } catch (error) {
-                        return Promise.reject(error);
-                    }
-                }
-            }
-            class StubTwitterAuthInfoClient {
-                getRecords(searchParams) {
-                    try {
-                        expect(searchParams.source).to.eql(sources.twitter.type);
-
-                        return Promise.resolve([]);
-                    } catch (error) {
-                        return Promise.reject(error);
-                    }
-                }
-            }
-
-            stubFacebookAuthInfoClientGetter = sinon.stub(FacebookSource, "AuthInfoClient").get(() => StubFacebookAuthInfoClient);
-            stubInstagramAuthInfoClientGetter = sinon.stub(InstagramSource, "AuthInfoClient").get(() => StubInstagramAuthInfoClient);
-            stubTwitterAuthInfoClientGetter = sinon.stub(TwitterSource, "AuthInfoClient").get(() => StubTwitterAuthInfoClient);
         });
 
         afterEach(function () {
-            stubFacebookAuthInfoClientGetter.restore();
-            stubInstagramAuthInfoClientGetter.restore();
-            stubTwitterAuthInfoClientGetter.restore();
             if (typeof originalFlickrApiKey === "undefined") {
                 delete process.env.FLICKR_API_KEY;
             } else {
@@ -70,7 +23,7 @@ describe("sources", function () {
         it("returns initialized sources", function () {
             return initializeSources()
                 .then(initializedSources => {
-                    const sourcesAsArray = Object.values(sources); // NOTE-RT: No guarantee that they'll be in the same order per the spec, but I assume they would be.
+                    const sourcesAsArray = Object.values(sources);
 
                     expect(initializedSources).to.have.length(sourcesAsArray.length);
                     initializedSources.forEach((initializedSource, index) => {
@@ -94,46 +47,12 @@ describe("sources", function () {
 
             return initializeSources(filter)
                 .then(initializedSources => {
-                    const sourcesAsArray = Object.values(sources); // NOTE-RT: No guarantee that they'll be in the same order per the spec, but I assume they would be.
+                    const sourcesAsArray = Object.values(sources);
 
                     expect(initializedSources).to.have.length(sourcesAsArray.length);
                     initializedSources.forEach((initializedSource, index) => {
                         expect(initializedSource).to.be.instanceof(sourcesAsArray[index]);
                     });
-                });
-        });
-
-        it("returns filtered initialized sources", function () {
-            stubInstagramAuthInfoClientGetter.restore();
-
-            const stubAuthInfos = [
-                new AuthInfo({id: "woof", token: "woof"}),
-                new AuthInfo({id: "meow", token: "meow"})
-            ];
-
-            class StubInstagramAuthInfoClient {
-                getRecords(searchParams) {
-                    try {
-                        expect(searchParams.source).to.eql(sources.instagram.type);
-
-                        return Promise.resolve(stubAuthInfos);
-                    } catch (error) {
-                        return Promise.reject(error);
-                    }
-                }
-            }
-
-            sinon.stub(InstagramSource, "AuthInfoClient").get(() => StubInstagramAuthInfoClient);
-
-            const filter = [sources.instagram.type];
-
-            return initializeSources(filter)
-                .then(initializedSources => {
-                    expect(initializedSources).to.have.length(stubAuthInfos.length);
-                    expect(initializedSources[0]).to.be.instanceof(sources[filter]);
-                    expect(initializedSources[0].authInfo).to.eql(stubAuthInfos[0]);
-                    expect(initializedSources[1]).to.be.instanceof(sources[filter]);
-                    expect(initializedSources[1].authInfo).to.eql(stubAuthInfos[1]);
                 });
         });
     });
