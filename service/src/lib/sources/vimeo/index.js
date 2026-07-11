741b57b7a588de963476a163fbffe1d995497ea1
Randy Tarampi <rt@randytarampi.ca>
2026-07-12 00:01:09 +0200

    feat(service): add Vimeo source for user videos
    Fetches videos from a Vimeo user via the Vimeo REST API with OAuth 2.0
    Bearer Token authentication.
    
    - VIMEO_ACCESS_TOKEN env var (required)
    - VIMEO_USER_ID env var (required)

---
+++ service/src/lib/sources/vimeo/index.js
@@ -0,0 +1,93 @@
+// @ts-check
+import {Post} from "@randy.tarampi/js";
+import CachedDataSource from "../../cachedDataSource.js";
+import {filterPostForOrderingConditionsInSearchParams} from "../util.js";
+
+/** @type {number} */
+const VIMEO_API_MAX_POSTS_PER_PAGE = 50;
+
+/** Vimeo-backed post source. */
+class VimeoSource extends CachedDataSource {
+    constructor(dataClient, cacheClient) {
+        super(dataClient || {}, cacheClient);
+    }
+
+    get isEnabled() {
+        return !!process.env.VIMEO_ACCESS_TOKEN && !!process.env.VIMEO_USER_ID || false;
+    }
+
+    static get type() {
+        return "vimeo";
+    }
+
+    static instanceToRecord(json) {
+        const pictures = json.pictures || {};
+        const sizes = pictures.sizes || [];
+        const bestPicture = sizes[sizes.length - 1] || {};
+
+        return Post.fromJS({
+            raw: json,
+            id: String(json.uri?.replace("/videos/", "") || json.id),
+            source: VimeoSource.type,
+            datePublished: json.created_time,
+            dateCreated: json.created_time,
+            title: json.name,
+            body: json.description || null,
+            sourceUrl: json.link,
+            creator: {
+                id: json.user?.uri?.replace("/users/", "") || json.user?.name || process.env.VIMEO_USER_ID || "vimeo",
+                username: json.user?.name || "vimeo",
+                url: json.user?.link || `https://vimeo.com/user${process.env.VIMEO_USER_ID}`
+            },
+            tags: json.tags?.map(t => t.name || t.tag) || []
+        });
+    }
+
+    async allRecordsGetter(searchParams) {
+        let posts = await this.recordsGetter(searchParams);
+
+        if (posts.length) {
+            posts = posts.concat(await this.allRecordsGetter(
+                searchParams
+                    .set("all", true)
+                    .set("page", searchParams.page + 1)
+            ));
+        }
+
+        return posts;
+    }
+
+    async recordsGetter(searchParams) {
+        const accessToken = process.env.VIMEO_ACCESS_TOKEN;
+        const userId = process.env.VIMEO_USER_ID;
+        const page = searchParams.page;
+        const perPage = Math.min(searchParams.perPage, VIMEO_API_MAX_POSTS_PER_PAGE);
+
+        const url = new URL(`https://api.vimeo.com/users/${userId}/videos`);
+        url.searchParams.set("page", String(page));
+        url.searchParams.set("per_page", String(perPage));
+
+        const response = await fetch(url.toString(), {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                Accept: "application/vnd.vimeo.*+json;version=3.4"
+            }
+        });
+
+        if (!response.ok) {
+            return [];
+        }
+
+        const data = await response.json();
+        const videos = data?.data || [];
+
+        return videos
+            .map(VimeoSource.instanceToRecord)
+            .filter(post => filterPostForOrderingConditionsInSearchParams(post, searchParams));
+    }
+}
+
+/** @type {typeof VimeoSource} */
+export default VimeoSource;
+
+export {VIMEO_API_MAX_POSTS_PER_PAGE};
---
+++ service/test/unit/src/lib/sources/vimeo/index.js
@@ -0,0 +1,287 @@
+import {Post, timedPromise} from "@randy.tarampi/js";
+import {expect} from "chai";
+import {DateTime} from "luxon";
+import sinon from "sinon";
+import PostSearchParams from "../../../../../../src/lib/postSearchParams.js";
+import VimeoSource, {VIMEO_API_MAX_POSTS_PER_PAGE} from "../../../../../../src/lib/sources/vimeo/index.js";
+import dummyClassesGenerator from "../../../../../lib/dummyClassesGenerator.js";
+
+describe("VimeoSource", function () {
+    let stubServiceClient;
+    let stubPost;
+    let stubPosts;
+    let stubBeforeRecordsGetter;
+    let stubRecordsGetter;
+    let stubAfterRecordsGetter;
+    let stubBeforeRecordGetter;
+    let stubRecordGetter;
+    let stubAfterRecordGetter;
+    let stubBeforeCachedRecordsGetter;
+    let stubCachedRecordsGetter;
+    let stubAfterCachedRecordsGetter;
+    let stubBeforeCachedRecordGetter;
+    let stubCachedRecordGetter;
+    let stubAfterCachedRecordGetter;
+    let stubInstanceToRecord;
+    let DummyCacheClient;
+    let stubCreateRecords;
+    let stubGetRecords;
+    let stubCreateRecord;
+    let stubGetRecord;
+    let stubCacheClient;
+    let builtDummyClasses;
+    let dummyClassBuilderArguments;
+
+    let vimeoVideo;
+    let vimeoVideos;
+    let vimeoApiResponse;
+
+    beforeEach(function () {
+        process.env.VIMEO_ACCESS_TOKEN = "vimeo-access-token";
+        process.env.VIMEO_USER_ID = "12345678";
+
+        stubPost = Post.fromJSON({id: "woof"});
+        stubPosts = [stubPost, Post.fromJSON({id: "meow"}), Post.fromJSON({id: "grr"})];
+
+        vimeoVideo = {
+            uri: "/videos/98765432",
+            name: "My First Vimeo Video",
+            description: "This is the description of my first Vimeo video.",
+            created_time: "2024-01-15T10:00:00+00:00",
+            link: "https://vimeo.com/98765432",
+            pictures: {
+                sizes: [
+                    {width: 100, height: 75, link: "https://i.vimeocdn.com/video/small.jpg"},
+                    {width: 640, height: 480, link: "https://i.vimeocdn.com/video/medium.jpg"}
+                ]
+            },
+            user: {
+                name: "Randy Tarampi",
+                link: "https://vimeo.com/randytarampi"
+            },
+            tags: [
+                {name: "tag1"},
+                {name: "tag2"}
+            ]
+        };
+        vimeoVideos = [vimeoVideo, {
+            ...vimeoVideo,
+            uri: "/videos/87654321",
+            name: "My Second Vimeo Video",
+            link: "https://vimeo.com/87654321"
+        }];
+
+        vimeoApiResponse = {
+            data: vimeoVideos
+        };
+
+        stubServiceClient = {};
+
+        stubBeforeRecordsGetter = sinon.stub().callsFake(params => timedPromise(params));
+        stubRecordsGetter = sinon.stub().callsFake(params => timedPromise(stubPosts)); // eslint-disable-line no-unused-vars
+        stubAfterRecordsGetter = sinon.stub().callsFake((posts, params) => timedPromise(posts)); // eslint-disable-line no-unused-vars
+
+        stubBeforeRecordGetter = sinon.stub().callsFake((postId, params) => timedPromise(params));
+        stubRecordGetter = sinon.stub().callsFake((postId, params) => timedPromise(stubPosts.find(post => post.id === postId) || null)); // eslint-disable-line no-unused-vars
+        stubAfterRecordGetter = sinon.stub().callsFake((post, params) => timedPromise(post)); // eslint-disable-line no-unused-vars
+
+        stubBeforeCachedRecordsGetter = sinon.stub().callsFake(params => timedPromise(params));
+        stubCachedRecordsGetter = sinon.stub().callsFake(params => timedPromise(stubPosts)); // eslint-disable-line no-unused-vars
+        stubAfterCachedRecordsGetter = sinon.stub().callsFake((posts, params) => timedPromise(posts)); // eslint-disable-line no-unused-vars
+
+        stubBeforeCachedRecordGetter = sinon.stub().callsFake((postId, params) => timedPromise(params));
+        stubCachedRecordGetter = sinon.stub().callsFake((postId, params) => timedPromise(stubPosts.find(post => post.id === postId) || null)); // eslint-disable-line no-unused-vars
+        stubAfterCachedRecordGetter = sinon.stub().callsFake((post, params) => timedPromise(post)); // eslint-disable-line no-unused-vars
+
+        stubInstanceToRecord = sinon.stub().callsFake(Post.fromJSON);
+
+        stubCreateRecords = sinon.stub().callsFake(posts => timedPromise(posts));
+        stubGetRecords = sinon.stub().callsFake(params => timedPromise(stubPosts)); // eslint-disable-line no-unused-vars
+
+        stubCreateRecord = sinon.stub().callsFake(post => timedPromise(post));
+        stubGetRecord = sinon.stub().callsFake(params => timedPromise(stubPost)); // eslint-disable-line no-unused-vars
+
+        dummyClassBuilderArguments = {
+            stubBeforeRecordsGetter,
+            stubRecordsGetter,
+            stubAfterRecordsGetter,
+
+            stubBeforeRecordGetter,
+            stubRecordGetter,
+            stubAfterRecordGetter,
+
+            stubBeforeCachedRecordsGetter,
+            stubCachedRecordsGetter,
+            stubAfterCachedRecordsGetter,
+
+            stubBeforeCachedRecordGetter,
+            stubCachedRecordGetter,
+            stubAfterCachedRecordGetter,
+
+            stubInstanceToRecord,
+
+            stubGetRecords,
+            stubCreateRecords,
+
+            stubGetRecord,
+            stubCreateRecord
+        };
+        builtDummyClasses = dummyClassesGenerator(dummyClassBuilderArguments);
+
+        DummyCacheClient = builtDummyClasses.DummyCacheClient;
+
+        stubCacheClient = new DummyCacheClient("ᶘ ◕ᴥ◕ᶅ");
+    });
+
+    describe("constructor", function () {
+        it("should build a `VimeoSource` instance (with stubbed client)", function () {
+            const vimeoSource = new VimeoSource(stubServiceClient, stubCacheClient);
+
+            expect(VimeoSource.type).to.eql("vimeo");
+            expect(vimeoSource.client).to.eql(stubServiceClient);
+            expect(vimeoSource.cacheClient).to.eql(stubCacheClient);
+            expect(vimeoSource.initializing).to.be.instanceOf(Promise);
+            expect(vimeoSource).to.be.instanceOf(VimeoSource);
+        });
+    });
+
+    describe("type", function () {
+        it("returns 'vimeo'", function () {
+            expect(VimeoSource.type).to.eql("vimeo");
+        });
+    });
+
+    describe("isEnabled", function () {
+        it("returns true when VIMEO_ACCESS_TOKEN and VIMEO_USER_ID are set", function () {
+            const vimeoSource = new VimeoSource(stubServiceClient, stubCacheClient);
+            expect(vimeoSource.isEnabled).to.be.true;
+        });
+
+        it("returns false when VIMEO_ACCESS_TOKEN is not set", function () {
+            delete process.env.VIMEO_ACCESS_TOKEN;
+            const vimeoSource = new VimeoSource(stubServiceClient, stubCacheClient);
+            expect(vimeoSource.isEnabled).to.be.false;
+        });
+
+        it("returns false when VIMEO_USER_ID is not set", function () {
+            delete process.env.VIMEO_USER_ID;
+            const vimeoSource = new VimeoSource(stubServiceClient, stubCacheClient);
+            expect(vimeoSource.isEnabled).to.be.false;
+        });
+    });
+
+    describe("instanceToRecord", function () {
+        it("turns a Vimeo video response into a `Post`", function () {
+            const post = VimeoSource.instanceToRecord(vimeoVideo);
+
+            expect(post).to.be.instanceOf(Post);
+            expect(post.id).to.eql("98765432");
+            expect(post.source).to.eql("vimeo");
+            expect(post.title).to.eql("My First Vimeo Video");
+            expect(post.body).to.eql("This is the description of my first Vimeo video.");
+            expect(post.sourceUrl).to.eql("https://vimeo.com/98765432");
+            expect(post.datePublished).to.be.instanceOf(DateTime);
+            expect(post.dateCreated).to.be.instanceOf(DateTime);
+            expect(post.creator.username).to.eql("Randy Tarampi");
+            expect(post.tags.toArray()).to.eql(["tag1", "tag2"]);
+        });
+    });
+
+    describe("recordsGetter", function () {
+        it("fetches videos from the Vimeo API and transforms them", async function () {
+            const vimeoSource = new VimeoSource(stubServiceClient, stubCacheClient);
+            const stubParams = PostSearchParams.fromJS({perPage: 10, page: 1});
+
+            const fetchStub = sinon.stub(globalThis, "fetch").resolves({
+                ok: true,
+                json: () => Promise.resolve(vimeoApiResponse)
+            });
+
+            try {
+                const posts = await vimeoSource.recordsGetter(stubParams);
+
+                expect(posts).to.be.instanceof(Array);
+                expect(posts).to.have.length(vimeoVideos.length);
+                posts.map(post => {
+                    expect(post).to.be.instanceof(Post);
+                });
+
+                const calledUrl = new URL(fetchStub.getCall(0).args[0]);
+                expect(calledUrl.hostname).to.eql("api.vimeo.com");
+                expect(calledUrl.pathname).to.eql("/users/12345678/videos");
+                expect(calledUrl.searchParams.get("page")).to.eql("1");
+                expect(calledUrl.searchParams.get("per_page")).to.eql("10");
+
+                const calledHeaders = fetchStub.getCall(0).args[1].headers;
+                expect(calledHeaders.Authorization).to.eql("Bearer vimeo-access-token");
+            } finally {
+                fetchStub.restore();
+            }
+        });
+
+        it("returns empty array when API call fails", async function () {
+            const vimeoSource = new VimeoSource(stubServiceClient, stubCacheClient);
+            const stubParams = PostSearchParams.fromJS({perPage: 10});
+
+            const fetchStub = sinon.stub(globalThis, "fetch").resolves({
+                ok: false,
+                status: 401
+            });
+
+            try {
+                const posts = await vimeoSource.recordsGetter(stubParams);
+                expect(posts).to.be.instanceof(Array);
+                expect(posts).to.be.empty;
+            } finally {
+                fetchStub.restore();
+            }
+        });
+    });
+
+    describe("allRecordsGetter", function () {
+        it("finds all videos", async function () {
+            const vimeoSource = new VimeoSource(stubServiceClient, stubCacheClient);
+            const stubParams = PostSearchParams.fromJS({perPage: 50});
+
+            const fetchStub = sinon.stub(globalThis, "fetch");
+
+            // First call returns videos
+            fetchStub.onCall(0).resolves({
+                ok: true,
+                json: () => Promise.resolve(vimeoApiResponse)
+            });
+            // Second call (recursive) returns empty
+            fetchStub.onCall(1).resolves({
+                ok: true,
+                json: () => Promise.resolve({data: []})
+            });
+
+            try {
+                const posts = await vimeoSource.allRecordsGetter(stubParams);
+
+                expect(posts).to.be.instanceof(Array);
+                expect(posts).to.have.length(vimeoVideos.length);
+                posts.map(post => {
+                    expect(post).to.be.instanceof(Post);
+                });
+            } finally {
+                fetchStub.restore();
+            }
+        });
+    });
+
+    describe("recordGetter", function () {
+        it("requires implementation", function () {
+            const vimeoSource = new VimeoSource(stubServiceClient, stubCacheClient);
+            expect(vimeoSource).to.be.instanceOf(VimeoSource);
+
+            return vimeoSource.recordGetter(stubPost.id, {})
+                .then(() => {
+                    throw new Error("Wtf? This should've thrown");
+                })
+                .catch(error => {
+                    expect(error.message).to.match(/Please specify an actual recordGetter implementation/);
+                });
+        });
+    });
+});
